675974ee491750a784fe8b8c05b724fb0b1c5803
Mark R. Evans <mark@glfusion.org>
2008-05-29 01:16:52 +0000

    Bumped version to 1.5.0rc1

--- public_html/lib-common.php
+++ public_html/lib-common.php
@@ -52,7 +52,7 @@ error_reporting( E_ERROR | E_WARNING | E_PARSE | E_COMPILE_ERROR );
 */
 
 if (!defined('glFusion_VERSION')) {
-  define('glFusion_VERSION', '1.5.0b1');
+  define('glFusion_VERSION', '1.5.0rc1');
 }
 
 /**
