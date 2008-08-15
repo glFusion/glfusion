539aa098fa028d96787e1ed27f102270e210e75a
Eric Warren <eakwarren@gmail.com>
2008-08-15 02:20:54 +0000

    Made ie6Warning javascript more neutral so it can be used by other themes

--- public_html/layout/nouveau/js/gl_mooie6warning.js
+++ public_html/layout/nouveau/js/gl_mooie6warning.js
@@ -12,13 +12,13 @@
 
 var IEWarn = new Class({
 	'initialize': function() {
-		var warning = "<h4>You are currently browsing this site with Internet Explorer 6 (IE6).<br />Due to limitiations in this browser, this site might not render as intended.</h4><p>The last version of IE6 was called Service Pack 1 for Internet Explorer 6 and was released in December of 2004.<br /><b>By continuing to run IE6 you are open to all security vulnerabilities discovered since that date.</b></p><p>In October of 2006, Microsoft released version 7 of Internet Explorer that provides better safety, and greater compliance with web standards. </p><p>On Feb 12th, 2008 Microsoft began to mandate updates to IE6 in order to move people towards the much improved and secure version 7. Microsoft currently considers IE7 to be a \"high-priority\" update.</p><h4>We encourage you to <a href=\"http://www.microsoft.com/windows/downloads/ie/getitnow.mspx\">update to Internet Explorer 7</a> for a better browsing experience.</h4>";
+		var warning = "<h4>You are currently browsing this site with Internet Explorer 6 (IE6).<br />Due to limitiations in this browser, this site might not render as intended.</h4><p>The last version of IE6 was called Service Pack 1 for Internet Explorer 6 and was released in December of 2004.<br /><b>By continuing to run IE6 you are open to all security vulnerabilities discovered since that date.</b></p><p>In October of 2006, Microsoft released version 7 of Internet Explorer that provides better safety and greater compliance with web standards. </p><p>On Feb 12th 2008, Microsoft began to mandate updates to IE6 in order to move people towards the much improved and secure version 7. Microsoft currently considers IE7 to be a \"high-priority\" update.</p><h4>We encourage you to <a href=\"http://www.microsoft.com/windows/downloads/ie/getitnow.mspx\">update to Internet Explorer 7</a> for a better browsing experience.</h4>";
 		
 		this.box = new Element('div', {'id': 'iewarn'}).inject(document.body, 'top');
 		var div = new Element('div').inject(this.box).setHTML(warning);
 		
 		var click = this.toggle.bind(this);
-		var button = new Element('a', {'id': 'iewarn_close'}).addEvents({
+		var button = new Element('a', {'id': 'iewarn_close', 'title': 'Close and don\'t remind me for 7 days'}).addEvents({
 			'click': function() {
 				click();	
 			}
