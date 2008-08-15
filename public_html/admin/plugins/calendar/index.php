e8eb4ce4aee2b3dafb004635a765308b73a4ec18
Mark R. Evans <mark@glfusion.org>
2008-08-15 01:57:12 +0000

    Fixed date comparison ("End date is before start date.")

--- public_html/admin/plugins/calendar/index.php
+++ public_html/admin/plugins/calendar/index.php
@@ -426,7 +426,8 @@ function CALENDAR_saveEvent ($eid, $title, $event_type, $url, $allday,
 
     // Make sure start date is before end date
     if (checkdate ($start_month, $start_day, $start_year)) {
-        $datestart = $start_year . '-' . $start_month . '-' . $start_day;
+        $datestart = sprintf('%4d-%02d-%02d',
+                             $start_year, $start_month, $start_day);
         $timestart = $start_hour . ':' . $start_minute . ':00';
     } else {
         $retval .= COM_siteHeader ('menu', $LANG_CAL_ADMIN[2]);
@@ -439,7 +440,7 @@ function CALENDAR_saveEvent ($eid, $title, $event_type, $url, $allday,
         return $retval;
     }
     if (checkdate ($end_month, $end_day, $end_year)) {
-        $dateend = $end_year . '-' . $end_month . '-' . $end_day;
+        $dateend = sprintf('%4d-%02d-%02d', $end_year, $end_month, $end_day);
         $timeend = $end_hour . ':' . $end_minute . ':00';
     } else {
         $retval .= COM_siteHeader ('menu', $LANG_CAL_ADMIN[2]);
