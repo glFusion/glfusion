c0c9cddcfd329032c8093deceed810f8c68039c2
Mark R. Evans <mark@glfusion.org>
2008-08-15 02:16:29 +0000

    Added popupWindow JS function

--- public_html/javascript/common.js
+++ public_html/javascript/common.js
@@ -53,9 +53,6 @@
        }
    }
 
-
-
-
 // Basic function to show/hide (toggle) an element - pass in the elment id
     function elementToggle(id) {
         var obj = document.getElementById(id);
@@ -77,4 +74,15 @@
         } else if (option == 'toggle') {
             elementToggle(id);
         }
+    }
+
+    /**
+    * Pops up a new window in the middle of the screen
+    */
+    function popupWindow(mypage, myname, w, h, scroll) {
+    	var winl = (screen.width - w) / 2;
+    	var wint = (screen.height - h) / 2;
+    	winprops = 'height='+h+',width='+w+',top='+wint+',left='+winl+',scrollbars='+scroll+',resizable'
+    	win = window.open(mypage, myname, winprops)
+    	if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
     }
