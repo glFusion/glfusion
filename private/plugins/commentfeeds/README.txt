311e60f24857bac3185c4394bb2dbc6a54b8756b
Mark R. Evans <mark@glfusion.org>
2008-08-15 02:36:07 +0000

    Added comment feeds plugin.

---
+++ private/plugins/commentfeeds/README.txt
@@ -0,0 +1,59 @@
+Comment Feeds Plugin for Geeklog
+--------------------------------
+
+  1) Install
+  2) Usage
+  3) Copyright
+  4) License
+  
+1. Installation
+===============
+
+Standard plugin install. Unzip the archive, copy the contents of the admin
+folder to /path/to/geeklog/public_html/admin/plugins/commentfeeds and the rest
+of the files to /path/to/geeklog/plugins/commentfeeds.
+
+Then, visit http://www.example.com/admin/plugins.php and click the install link
+for the commentfeeds plugin.
+
+2. Usage
+========
+
+The plugin itself has no admin or anything at all. All you need to do is go to
+your geeklog instance's syndication feed admin and add a new Commentfeed feed,
+you can have it cover comments against all topics, or just comments for a
+particular topic. You may have as many feeds as you like. Exactly like the
+feeds for articles, but only containing comments.
+
+You should choose a type of RSS 2.0.
+
+The system will only list comments to articles that the anonymous user has
+access to, this includes requiring access to the appropriate geeklog topic.
+
+3. Copyright
+============
+
+Copyright (c) 2006 Michael Jervis (mike@fuckingbrit.com) all rights reserved.
+
+Uses the Geeklog generic plugin installer, as used by the core Geeklog Links
+plugin, which is copyright by it's orginal authors. (If somewhat cut down in
+this plugin)
+
+4. License
+==========
+
+All code released under the GPL, as is Geeklog:
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
---
+++ public_html/admin/plugins/commentfeeds/install.php
@@ -0,0 +1,96 @@
+<?php
+// +--------------------------------------------------------------------------+
+// | CommentFeeds Plugin - glFusion CMS                                       |
+// +--------------------------------------------------------------------------+
+// | install.php                                                              |
+// |                                                                          |
+// | Installation                                                             |
+// +--------------------------------------------------------------------------+
+// | $Id::                                                                   $|
+// +--------------------------------------------------------------------------+
+// | Copyright (C) 2002-2008 by the following authors:                        |
+// |                                                                          |
+// | Mark R. Evans          mark AT glfusion DOT org                          |
+// |                                                                          |
+// | Based on the Comment Feed Plugin for Geeklog                             |
+// | Author:                                                                  |
+// | Michael Jervis <mike AT fuckingbrit DOT com>                             |
+// +--------------------------------------------------------------------------+
+// |                                                                          |
+// | This program is free software; you can redistribute it and/or            |
+// | modify it under the terms of the GNU General Public License              |
+// | as published by the Free Software Foundation; either version 2           |
+// | of the License, or (at your option) any later version.                   |
+// |                                                                          |
+// | This program is distributed in the hope that it will be useful,          |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
+// | GNU General Public License for more details.                             |
+// |                                                                          |
+// | You should have received a copy of the GNU General Public License        |
+// | along with this program; if not, write to the Free Software Foundation,  |
+// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
+// |                                                                          |
+// +--------------------------------------------------------------------------+
+
+require_once('../../../lib-common.php');
+require_once($_CONF['path'] . '/plugins/commentfeeds/config.php');
+require_once($_CONF['path'] . '/plugins/commentfeeds/functions.inc');
+require_once($_CONF['path'] . '/plugins/commentfeeds/install.inc');
+
+// Only let Root users access this page
+if (!SEC_inGroup('Root')) {
+    // Someone is trying to illegally access this page
+    COM_errorLog("Someone has tried to illegally access the CommentFeeds install/uninstall page.  User id: {$_USER['uid']}, Username: {$_USER['username']}, IP: $REMOTE_ADDR",1);
+    $display = COM_siteHeader();
+    $display .= COM_startBlock($LANG27[12]);
+    $display .= $LANG_ACCESS['plugin_access_denied_msg'];
+    $display .= COM_endBlock();
+    $display .= COM_siteFooter(true);
+    echo $display;
+    exit;
+}
+
+// MAIN
+$display = '';
+
+if ($_REQUEST['action'] == 'uninstall') {
+    $uninstall_plugin = 'plugin_uninstall_' . $pi_name;
+    if ($uninstall_plugin ()) {
+        $display = COM_refresh ($_CONF['site_admin_url']
+                                . '/plugins.php?msg=45');
+    } else {
+        $display = COM_refresh ($_CONF['site_admin_url']
+                                . '/plugins.php?msg=73');
+    }
+
+} else if (DB_count ($_TABLES['plugins'], 'pi_name', $pi_name) == 0) {
+    // plugin not installed
+    if (commentfeeds_compatible_with_this_glfusion_version ()) {
+        if (plugin_install_commentfeeds ($_DB_table_prefix)) {
+            $display = COM_refresh ($_CONF['site_admin_url']
+                                    . '/plugins.php?msg=44');
+        } else {
+            $display = COM_refresh ($_CONF['site_admin_url']
+                                    . '/plugins.php?msg=72');
+        }
+    } else {
+        // plugin needs a newer version of glFusion
+        $display .= COM_siteHeader ('menu', $LANG32[8])
+                 . COM_startBlock ($LANG32[8])
+                 . '<p>' . $LANG32[9] . '</p>'
+                 . COM_endBlock ()
+                 . COM_siteFooter ();
+    }
+} else {
+    // plugin already installed
+    $display .= COM_siteHeader ('menu', $LANG01[77])
+             . COM_startBlock ($LANG32[6])
+             . '<p>' . $LANG32[7] . '</p>'
+             . COM_endBlock ()
+             . COM_siteFooter();
+}
+
+echo $display;
+
+?>
