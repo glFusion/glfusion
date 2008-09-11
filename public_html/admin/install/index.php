a99ed34c20777532ffc8846fb4815a45dc00f26b
Mark R. Evans <mark@glfusion.org>
2008-09-11 02:39:44 +0000

    Updated upgrade routines

--- public_html/admin/install/index.php
+++ public_html/admin/install/index.php
@@ -1121,8 +1121,37 @@ function INST_doDatabaseUpgrades($current_fusion_version, $use_innodb = false)
 
             $c->add('hide_adminmenu',TRUE,'select',3,12,1,1170,TRUE);
 
+            // search stuff (temp for now)
+            $c->add('fs_search', NULL, 'fieldset', 0, 6, NULL, 0, TRUE);
+            $c->add('search_style','google','select',0,6,18,650,TRUE);
+            $c->add('search_limits','10,15,25,30','text',0,6,NULL,660,TRUE);
+            $c->add('num_search_results',30,'text',0,6,NULL,670,TRUE);
+            $c->add('search_show_limit',TRUE,'select',0,6,1,680,TRUE);
+            $c->add('search_show_sort',TRUE,'select',0,6,1,690,TRUE);
+            $c->add('search_show_num',TRUE,'select',0,6,1,700,TRUE);
+            $c->add('search_show_type',TRUE,'select',0,6,1,710,TRUE);
+            $c->add('search_show_user',TRUE,'select',0,6,1,720,TRUE);
+            $c->add('search_show_hits',TRUE,'select',0,6,1,730,TRUE);
+            $c->add('search_no_data','<i>Not available...</i>','text',0,6,NULL,740,TRUE);
+            $c->add('search_separator',' &gt; ','text',0,6,NULL,750,TRUE);
+            $c->add('search_def_keytype','phrase','select',0,6,19,760,TRUE);
+            $c->restore_param('num_search_results', 'Core');
+
+            // This option should only be set during the install/upgrade because of all
+            // the setting up thats required. So hide it from the user.
+            $c->add('search_use_fulltext',FALSE,'hidden',0,6);
+
             $current_fusion_version = '1.1.0';
             $_SQL = '';
+
+            if (INST_pluginExists('staticpages')) {
+                $check = upgrade_StaticpagesPlugin();
+                if (!$check) {
+                    echo "Error updating the staticpages";
+                    return false;
+                }
+            }
+
             break;
 
         default:
