1d0355886c05b7c2c537aa543118458600397d7d
Mark R. Evans <mark@glfusion.org>
2008-09-16 18:01:27 +0000

    Fixed a couple of fatal typos

--- public_html/filemgmt/index.php
+++ public_html/filemgmt/index.php
@@ -105,7 +105,7 @@ if (SEC_hasRights('filemgmt.user') OR $mydownloads_publicpriv == 1) {
         $result2 = DB_query("SELECT username,fullname,photo FROM {$_TABLES['users']} WHERE uid = $submitter");
         list ($submitter_name,$submitter_fullname,$photo) = DB_fetchARRAY($result2);
         $submitter_name = COM_getDisplayName ($submitter, $submitter_name, $submitter_fullname);
-        include($_CONF['path'] .'plugins/filemgmt/include/dlformat.php';
+        include $_CONF['path'] .'plugins/filemgmt/include/dlformat.php';
 
         $p->set_var('cssid',1);
         $p->parse ('filelisting_records', 'records');
@@ -255,7 +255,7 @@ if (SEC_hasRights('filemgmt.user') OR $mydownloads_publicpriv == 1) {
                     $result2 = DB_query("SELECT username,fullname,photo  FROM {$_TABLES['users']} WHERE uid = $submitter");
                     list ($submitter_name,$submitter_fullname,$photo) = DB_fetchARRAY($result2);
                     $submitter_name = COM_getDisplayName ($submitter, $submitter_name, $submitter_fullname);
-                    include($_CONF['path'] .'plugins/filemgmt/include/dlformat.php';
+                    include $_CONF['path'] .'plugins/filemgmt/include/dlformat.php';
                     $p->set_var('cssid',$cssid);
                     $p->parse ('filelisting_records', 'records',true);
                     $cssid = ($cssid == 2) ? 1 : 2;
