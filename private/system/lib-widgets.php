acd39e8d346577f23a9146789a968d33d8be4ff1
Eric Warren <eakwarren@gmail.com>
2008-10-12 23:25:08 +0000

    mooslide titles can now have apostrophes, etc. (double check my code guys! :-)

--- private/system/lib-widgets.php
+++ private/system/lib-widgets.php
@@ -75,9 +75,10 @@ EOJ;
     $res = DB_query($sql);
     for ($i = 0; $A = DB_fetchArray($res); ++$i) {
         $content = SP_render_content(stripslashes($A['sp_content']), $A['sp_php']);
+		$title = SP_render_content(stripslashes($A['sp_title']), $A{'sp_php'});
         $display .= <<<EOS
 <div class="tab-pane" id="tab-$i-pane">
-	<h1 class="tab-title">{$A['sp_title']}</h1>
+	<h1 class="tab-title">$title</h1>
 	<div>$content</div>
 </div>
 EOS;
@@ -118,7 +119,7 @@ function WIDGET_mootickerRSS($block = 'gl_mootickerRSS', $id = 'gl_mooticker') {
 var MooTicker=new Class({options:{controls:true,delay:2000,duration:800,blankimage:'{site_url}/images/speck.gif'},initialize:function(b,c){this.setOptions(c);this.element=\$(b)||null;this.element.addEvents({'mouseenter':this.stop.bind(this),'mouseleave':this.play.bind(this)});this.news=this.element.getElements('ul li');this.current=0;this.fx=[];this.news.getParent().setStyle('position','relative');var d=this;this.news.each(function(a,i){a.setStyle('position','absolute');this.fx[i]=new Fx.Style(a,'opacity',{duration:this.options.duration,onStart:function(){d.transitioning=true},onComplete:function(){d.transitioning=false}}).set(1);if(!i)return;a.setStyle('opacity',0)},this);if(this.options.controls)this.addControls();this.status='stop';window.addEvent('load',this.play.bind(this));return this},addControls:function(){var a=new Element('span',{'class':'controls'}).injectTop(this.element);this.arrowPrev=new Element('img',{'class':'control-prev','title':'Previous','alt':'prev','src':this.options.blankimage}).inject(a);this.arrowNext=new Element('img',{'class':'control-next','title':'Next','alt':'next','src':this.options.blankimage}).inject(a);this.arrowPrev.addEvent('click',this.previous.bind(this));this.arrowNext.addEvent('click',this.next.bind(this));return this},previous:function(){if(this.transitioning)return this;var a=(!this.current)?this.news.length-1:this.current-1;this.fx[this.current].start(0);this.fx[a].start(1);this.current=a;return this},next:function(){if(this.transitioning)return this;var a=(this.current==this.news.length-1)?0:this.current+1;this.fx[this.current].start(0);this.fx[a].start(1);this.current=a;return this},play:function(){if(this.status=='play')return this;this.status='play';this.timer=this.next.periodical(this.options.delay+this.options.duration,this);return this},stop:function(){this.status='stop';\$clear(this.timer);return this}});MooTicker.implement(new Options,new Events);
 </script>
 <script type="text/javascript">
-         window.addEvent('load', function() {
+         window.addEvent('domready', function() { //leave as domready so list won't display until ready
                  var x = new MooTicker('$id', {
                          controls: true,
                          delay: 2500,
