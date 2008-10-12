65004276292f3df34a2601a79d36317906bbb7a4
jmucchiello <jmucchiello@users.sourceforge.net>
2008-10-12 03:08:02 +0000

    Parameterized WIDGET_mootickerRSS() Can now specify block name and HTML id.

--- private/system/lib-widgets.php
+++ private/system/lib-widgets.php
@@ -103,11 +103,10 @@ echo $display;
 
 // A portal staticpage (that bases itself on a user created proper portal block called gl_mootickerRSS)
 //modified from LWC's forum post http://www.geeklog.net/forum/viewtopic.php?showtopic=67396 by Mark R. Evans and Joe Mucchiello
-function WIDGET_mootickerRSS() {
+function WIDGET_mootickerRSS($block = 'gl_mootickerRSS', $id = 'gl_mooticker') {
 	global $_CONF, $_TABLES, $LANG_WIDGETS;
 	$retval = '';
-	$result = DB_query("SELECT *, rdfupdated as date FROM
-{$_TABLES['blocks']} WHERE name='gl_mootickerRSS'");
+	$result = DB_query("SELECT *, rdfupdated as date FROM {$_TABLES['blocks']} WHERE name='" . $block . "'");
 	$numRows = DB_numRows($result);
 	if ( $numRows < 1 || $result == NULL ) {
 		return $retval;
@@ -116,20 +115,20 @@ function WIDGET_mootickerRSS() {
 	if ( $B['is_enabled'] == 0 ) {
 		$retval = <<<EOT
 <script type="text/javascript">
-var MooTicker=new Class({options:{controls:true,delay:2000,duration:800,blankimage:'{site_url}/images/speck.gif'},initialize:function(b,c){this.setOptions(c);this.element=$(b)||null;this.element.addEvents({'mouseenter':this.stop.bind(this),'mouseleave':this.play.bind(this)});this.news=this.element.getElements('ul li');this.current=0;this.fx=[];this.news.getParent().setStyle('position','relative');var d=this;this.news.each(function(a,i){a.setStyle('position','absolute');this.fx[i]=new Fx.Style(a,'opacity',{duration:this.options.duration,onStart:function(){d.transitioning=true},onComplete:function(){d.transitioning=false}}).set(1);if(!i)return;a.setStyle('opacity',0)},this);if(this.options.controls)this.addControls();this.status='stop';window.addEvent('load',this.play.bind(this));return this},addControls:function(){var a=new Element('span',{'class':'controls'}).injectTop(this.element);this.arrowPrev=new Element('img',{'class':'control-prev','title':'Previous','alt':'prev','src':this.options.blankimage}).inject(a);this.arrowNext=new Element('img',{'class':'control-next','title':'Next','alt':'next','src':this.options.blankimage}).inject(a);this.arrowPrev.addEvent('click',this.previous.bind(this));this.arrowNext.addEvent('click',this.next.bind(this));return this},previous:function(){if(this.transitioning)return this;var a=(!this.current)?this.news.length-1:this.current-1;this.fx[this.current].start(0);this.fx[a].start(1);this.current=a;return this},next:function(){if(this.transitioning)return this;var a=(this.current==this.news.length-1)?0:this.current+1;this.fx[this.current].start(0);this.fx[a].start(1);this.current=a;return this},play:function(){if(this.status=='play')return this;this.status='play';this.timer=this.next.periodical(this.options.delay+this.options.duration,this);return this},stop:function(){this.status='stop';$clear(this.timer);return this}});MooTicker.implement(new Options,new Events);
+var MooTicker=new Class({options:{controls:true,delay:2000,duration:800,blankimage:'{site_url}/images/speck.gif'},initialize:function(b,c){this.setOptions(c);this.element=\$(b)||null;this.element.addEvents({'mouseenter':this.stop.bind(this),'mouseleave':this.play.bind(this)});this.news=this.element.getElements('ul li');this.current=0;this.fx=[];this.news.getParent().setStyle('position','relative');var d=this;this.news.each(function(a,i){a.setStyle('position','absolute');this.fx[i]=new Fx.Style(a,'opacity',{duration:this.options.duration,onStart:function(){d.transitioning=true},onComplete:function(){d.transitioning=false}}).set(1);if(!i)return;a.setStyle('opacity',0)},this);if(this.options.controls)this.addControls();this.status='stop';window.addEvent('load',this.play.bind(this));return this},addControls:function(){var a=new Element('span',{'class':'controls'}).injectTop(this.element);this.arrowPrev=new Element('img',{'class':'control-prev','title':'Previous','alt':'prev','src':this.options.blankimage}).inject(a);this.arrowNext=new Element('img',{'class':'control-next','title':'Next','alt':'next','src':this.options.blankimage}).inject(a);this.arrowPrev.addEvent('click',this.previous.bind(this));this.arrowNext.addEvent('click',this.next.bind(this));return this},previous:function(){if(this.transitioning)return this;var a=(!this.current)?this.news.length-1:this.current-1;this.fx[this.current].start(0);this.fx[a].start(1);this.current=a;return this},next:function(){if(this.transitioning)return this;var a=(this.current==this.news.length-1)?0:this.current+1;this.fx[this.current].start(0);this.fx[a].start(1);this.current=a;return this},play:function(){if(this.status=='play')return this;this.status='play';this.timer=this.next.periodical(this.options.delay+this.options.duration,this);return this},stop:function(){this.status='stop';\$clear(this.timer);return this}});MooTicker.implement(new Options,new Events);
 </script>
 <script type="text/javascript">
          window.addEvent('load', function() {
-                 var x = new MooTicker('gl_mooticker', {
+                 var x = new MooTicker('$id', {
                          controls: true,
                          delay: 2500,
                          duration: 600 });
          });
 </script>
-<div id="gl_mooticker">
+<div id="$id">
 EOT;
 		$retval = str_replace('{site_url}',$_CONF['site_url'] , $retval);
-		$retval .= '<span class="tickertitle">' . $LANG_WIDGETS[latest_news] . ':</span>';
+		$retval .= '<span class="tickertitle">' . $LANG_WIDGETS['latest_news'] . ':</span>';
 		$retval .= $B['content'] . '</div>';
 	}
 	return $retval;
