cf92eb21e733db76d91213f881a70d285f2c1eda
Mark R. Evans <mark@glfusion.org>
2008-11-22 21:14:42 +0000

    Upgraded BB2 to v2.0.25

--- public_html/bad_behavior2/bad-behavior/blackhole.inc.php
+++ public_html/bad_behavior2/bad-behavior/blackhole.inc.php
@@ -42,7 +42,7 @@ function bb2_httpbl($settings, $package) {
 	$result = gethostbynamel($settings['httpbl_key'].".${find}.dnsbl.httpbl.org.");
 	if (!empty($result)) {
 		$ip = explode('.', $result[0]);
-		if ($ip[0] == 127 && ($ip[3] & 7) && $ip[2] >= $settings['httpbl_threat'] && $ip[1] >= $settings['httpbl_maxage']) {
+		if ($ip[0] == 127 && ($ip[3] & 7) && $ip[2] >= $settings['httpbl_threat'] && $ip[1] <= $settings['httpbl_maxage']) {
 			return '2b021b1f';
 		}
 	}
