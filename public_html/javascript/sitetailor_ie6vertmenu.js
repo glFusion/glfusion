891c619d508bc9f9671626e45be4b0353b787ad5
Eric Warren <eakwarren@gmail.com>
2008-11-23 05:50:30 +0000

    fixed script so it works for left block menu again (will have to manually edit for sites that use the menu on the right)

--- public_html/javascript/sitetailor_ie6vertmenu.js
+++ public_html/javascript/sitetailor_ie6vertmenu.js
@@ -1,7 +1,7 @@
 // code for IE6 for Site Tailor vertical menus only
 STverticalmenu = function() {
 	if(!document.body.currentStyle) return;
-	var subs = document.getElementsByName('parent');
+	var subs = document.getElementsByName('parent-l3'); //set to parent-r3 if using in #gl_extra column
 	for(var i=0; i<subs.length; i++) {
 		var li = subs[i].parentNode;
 		if(li && li.lastChild.style) {
