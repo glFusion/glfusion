f0433198c07d13afc060b9dd03a5f3a26bc53761
Eric Warren <eakwarren@gmail.com>
2008-11-24 05:58:24 +0000

    initial import of Novus theme

---
+++ public_html/layout/novus/functions.php
@@ -0,0 +1,85 @@
+<?php
+// +--------------------------------------------------------------------------+
+// | glFusion CMS                                                             |
+// +--------------------------------------------------------------------------+
+// | functions.php                                                            |
+// |                                                                          |
+// | Theme specific functions                                                 |
+// +--------------------------------------------------------------------------+
+// | $Id:: functions.php 3363 2008-10-11 01:16:35Z ewarren                   $|
+// +--------------------------------------------------------------------------+
+// | Copyright (C) 2002-2008 by the following authors:                        |
+// |                                                                          |
+// | Mark R. Evans          mark AT glfusion DOT org                          |
+// |                                                                          |
+// +--------------------------------------------------------------------------+
+// |                                                                          |
+// | This program is free software; you can redistribute it and/or            |
+// | modify it under the terms of the GNU General Public License              |
+// | as published by the Free Software Foundation; either version 2           |
+// | of the License, or (at your option) any later version.                   |
+// |                                                                          |
+// | This program is distributed in the hope that it will be useful,          |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
+// | GNU General Public License for more details.                             |
+// |                                                                          |
+// | You should have received a copy of the GNU General Public License        |
+// | along with this program; if not, write to the Free Software Foundation,  |
+// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
+// |                                                                          |
+// +--------------------------------------------------------------------------+
+
+// this file can't be used on its own
+if (!defined ('GVERSION')) {
+    die ('This file can not be used on its own!');
+}
+
+//set glFusion style COM_siteHeader/Footer functions.
+//Comment out to use old Geeklog functions.
+$themeAPI = 2; 
+
+$_IMAGE_TYPE = 'png';
+
+if (!defined ('XHTML')) {
+    define('XHTML',' /'); // change this to ' /' for XHTML, and '' for HTML. Don't forget to update your doctype in htmlheader.thtml.
+}
+$lang = COM_getLanguageId();
+if (empty($lang)) {
+    $result = DB_query("SELECT onleft,name FROM {$_TABLES['blocks']} WHERE is_enabled = 1");
+} else {
+    $result = DB_query("SELECT onleft,name FROM {$_TABLES['blocks']}");
+}
+$nrows = DB_numRows($result);
+for ($i = 0; $i < $nrows; $i++) {
+    $A = DB_fetchArray($result);
+        if ($A['onleft'] == 1) {
+            $_BLOCK_TEMPLATE[$A['name']] = 'blockheader-left.thtml,blockfooter-left.thtml';
+        } else {
+            $_BLOCK_TEMPLATE[$A['name']] = 'blockheader-right.thtml,blockfooter-right.thtml';
+    }
+}
+$_BLOCK_TEMPLATE['_msg_block'] = 'blockheader-message.thtml,blockfooter-message.thtml';
+$_BLOCK_TEMPLATE['whats_related_block'] = 'blockheader-related.thtml,blockfooter-related.thtml';
+$_BLOCK_TEMPLATE['story_options_block'] = 'blockheader-related.thtml,blockfooter-related.thtml';
+// Define the blocks that are a list of links styled as an unordered list - using class="blocklist"
+$_BLOCK_TEMPLATE['admin_block'] = 'blockheader-list.thtml,blockfooter-list.thtml';
+$_BLOCK_TEMPLATE['section_block'] = 'blockheader-list.thtml,blockfooter-list.thtml';
+// $_BLOCK_TEMPLATE['user_block'] = 'blockheader-list.thtml,blockfooter-list.thtml';
+$_BLOCK_TEMPLATE['forum_menu'] = 'blockheader-left.thtml,blockfooter-left.thtml';
+// $_BLOCK_TEMPLATE['configmanager_block'] = 'blockheader-left.thtml,blockfooter-left.thtml';
+// $_BLOCK_TEMPLATE['configmanager_subblock'] = 'blockheader-left.thtml,blockfooter-left.thtml';
+
+
+function novus_themeJS() {
+    global $_CONF;
+
+    $js = array();
+// uncomment the line below to enable gl_moochronometer header rotator
+// MAKE SURE TO CLEAR BROWSER & C.T.L. CACHE when activating/deactivating
+//    $js[] = $_CONF['path_html'] .'javascript/mootools/gl_moochronometer.js';
+    $js[] = $_CONF['path_layout'] .'js/gltips.js';
+
+    return($js);
+}
+?>
