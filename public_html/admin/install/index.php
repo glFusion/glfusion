6b6cd404b33da27a21991ee04c19056a1161a8d0
Mark R. Evans <mark@glfusion.org>
2009-02-23 22:00:17 +0000

    Touch (create) all log files after installation.

--- public_html/admin/install/index.php
+++ public_html/admin/install/index.php
@@ -1352,6 +1352,11 @@ function INST_installAndContentPlugins()
 
     $config->_purgeCache();
 
+    @touch($log_path.'error.log');
+    @touch($log_path.'access.log');
+    @touch($log_path.'captcha.log');
+    @touch($log_path.'spamx.log');
+
     global $_CONF, $_SYSTEM, $_DB, $_GROUPS, $_RIGHTS, $TEMPLATE_OPTIONS;
 
     require $_CONF['path_html'].'lib-common.php';
