faae00c0f601195d0a56f08be54920064a05969a
Mark R. Evans <mark@glfusion.org>
2009-09-14 02:31:32 +0000

    Use same JS to insert smileys

--- public_html/forum/javascript/createtopic.js
+++ public_html/forum/javascript/createtopic.js
@@ -76,16 +76,8 @@ function checkForm() {
 }
 
 function emoticon(text) {
-    var txtarea = document.forumpost.comment;
-    text = ' ' + text + ' ';
-    if (txtarea.createTextRange && txtarea.caretPos) {
-        var caretPos = txtarea.caretPos;
-        caretPos.text = caretPos.text.charAt(caretPos.text.length - 1) == ' ' ? caretPos.text + text + ' ' : caretPos.text + text;
-        txtarea.focus();
-    } else {
-        txtarea.value  += text;
-        txtarea.focus();
-    }
+
+    bbfontstyle(text,'');
 }
 
 /**
