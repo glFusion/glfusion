f7524637072c339ceb631da3f89abbf6bb24efde
Mark R. Evans <mark@glfusion.org>
2009-11-24 16:47:35 +0000

    Bump Forum version

--- private/plugins/forum/forum.php
+++ private/plugins/forum/forum.php
@@ -49,8 +49,8 @@ $_FF_CONF = array();
 
 $_FF_CONF['pi_name']            = 'forum';
 $_FF_CONF['pi_display_name']    = 'Forum';
-$_FF_CONF['pi_version']         = '3.1.5';
-$_FF_CONF['gl_version']         = '1.1.6';
+$_FF_CONF['pi_version']         = '3.1.6';
+$_FF_CONF['gl_version']         = '1.1.7';
 $_FF_CONF['pi_url']             = 'http://www.glfusion.org/';
 
 $_TABLES['gf_userprefs']    = $_DB_table_prefix . 'forum_userprefs';
