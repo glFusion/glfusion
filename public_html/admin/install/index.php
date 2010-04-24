91751d5a6431663723f490c7c2500323fd1e6358
Mark R. Evans <mark@glfusion.org>
2010-04-24 14:45:48 +0000

    Set image_lib when migrating a site.

--- public_html/admin/install/index.php
+++ public_html/admin/install/index.php
@@ -1718,6 +1718,7 @@ function INST_doSiteUpgrade()
                 $c->add('comment_edittime',1800,'text',4,21,NULL,1690,TRUE);
                 $c->add('article_comment_close_days',30,'text',4,21,NULL,1700,TRUE);
                 $c->add('comment_close_rec_stories',0,'text',4,21,NULL,1710,TRUE);
+                $c->add('image_lib','gdlib','select',5,22,10,1450,TRUE);
                 $c->add('jhead_enabled',0,'select',5,22,0,1480,TRUE);
                 $c->add('path_to_jhead','','text',5,22,NULL,1490,TRUE);
                 $c->add('jpegtrans_enabled',0,'select',5,22,0,1500,TRUE);
