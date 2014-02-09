97276a09795b525a4ce63b63bc809bd822885d0b
Mark R. Evans <mark@glfusion.org>
2014-02-09 10:23:20 -0600

    Updated header.

--- public_html/admin/plugins/captcha/install.php
+++ public_html/admin/plugins/captcha/install.php
@@ -6,9 +6,7 @@
 // |                                                                          |
 // | Installs / uninstalls plugin                                             |
 // +--------------------------------------------------------------------------+
-// | $Id::                                                                   $|
-// +--------------------------------------------------------------------------+
-// | Copyright (C) 2006-2008 by the following authors:                        |
+// | Copyright (C) 2006-2014 by the following authors:                        |
 // |                                                                          |
 // | Mark R. Evans          mark AT glfusion DOT org                          |
 // |                                                                          |
@@ -37,7 +35,7 @@ USES_lib_install();
 
 if (!SEC_inGroup('Root')) {
     // Someone is trying to illegally access this page
-    COM_errorLog("Someone has tried to illegally access the CAPTCHA install/uninstall page.  User id: {$_USER['uid']}, Username: {$_USER['username']}, IP: $REMOTE_ADDR",1);
+    COM_errorLog("Someone has tried to access the CAPTCHA install/uninstall page without the proper permissions.  User id: {$_USER['uid']}, Username: {$_USER['username']}, IP: $REMOTE_ADDR",1);
     $display = COM_siteHeader ('menu', $LANG_ACCESS['accessdenied'])
              . COM_startBlock ($LANG_ACCESS['accessdenied'])
              . $LANG_ACCESS['plugin_access_denied_msg']
