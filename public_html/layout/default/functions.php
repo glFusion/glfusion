8fa4e3f49ad91ecd44015a99dbab47d61bc78259
Mark R. Evans <mark@glfusion.org>
2014-06-13 13:13:04 -0500

    Updated to support jQuery core

--- public_html/layout/default/functions.php
+++ public_html/layout/default/functions.php
@@ -1,32 +1,14 @@
 <?php
-// +--------------------------------------------------------------------------+
-// | glFusion CMS                                                             |
-// +--------------------------------------------------------------------------+
-// | functions.php                                                            |
-// |                                                                          |
-// | Support functions for Newvou theme                                       |
-// +--------------------------------------------------------------------------+
-// | Copyright (C) 2008-2014 by the following authors:                        |
-// |                                                                          |
-// | Mark R. Evans          mark AT glfusion DOT org                          |
-// |                                                                          |
-// +--------------------------------------------------------------------------+
-// |                                                                          |
-// | This program is free software; you can redistribute it and/or            |
-// | modify it under the terms of the GNU General Public License              |
-// | as published by the Free Software Foundation; either version 2           |
-// | of the License, or (at your option) any later version.                   |
-// |                                                                          |
-// | This program is distributed in the hope that it will be useful,          |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with this program; if not, write to the Free Software Foundation,  |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
-// |                                                                          |
-// +--------------------------------------------------------------------------+
+/**
+* @package  glFusion CMS
+*
+* @license GNU General Public License version 2 or later
+*     http://www.opensource.org/licenses/gpl-license.php
+*
+*  Copyright (C) 2010-2014 by the following authors:
+*   Mark R. Evans   mark AT glfusion DOT org
+*
+*/
 
 // this file can't be used on its own
 if (!defined ('GVERSION')) {
@@ -64,49 +46,21 @@ $_BLOCK_TEMPLATE['forum_menu'] = 'blockheader-left.thtml,blockfooter-left.thtml'
 
 // define the JS we need for this theme..
 $outputHandle = outputHandler::getInstance();
-// core js
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/jquery-1.11.0.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/jquery-ui-1.10.4.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/hoverIntent.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/jqrating.js');
 
-// tooltips
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/jquery.tooltipster.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/tooltip.js');
-// menu animation
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/superfish.js');
-// lightbox
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/slimbox2.js');
-// validation
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/jquery.validate.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/additional-methods.min.js');
 // slideshow
 $outputHandle->addScriptFile($_CONF['path_layout'].'js/jquery.tcycle.js');
-// tooltips
-$outputHandle->addLinkStyle($_CONF['layout_url'].'/css/tooltipster.css');
-//slimbox
-$outputHandle->addLinkStyle($_CONF['layout_url'].'/css/slimbox2.css');
+// cookie law
+$outputHandle->addScriptFile($_CONF['path_layout'].'js/jquery.cookiebar.js');
+$outputHandle->addLinkStyle($_CONF['layout_url'].'/css/jquery.cookiebar.css');
+
+// must load the jquery ui library we want to use.
+$outputHandle->addLinkStyle($_CONF['layout_url'].'/css/ui-lightness/jquery-ui-1.10.4.min.css');
+
+
+
 
 function theme_getToolTipStyle()
 {
     return('tooltip');
-}
-
-// compatibility
-
-if ( !function_exists('WIDGET_mooslide') ) {
-    function WIDGET_mooslide($page_ids, $width = 550, $height = 160, $id = 'gl_', $slide_interval = 0) {}
-}
-if ( !function_exists('WIDGET_mootickerRSS') ) {
-    function WIDGET_mootickerRSS($block = 'gl_mootickerRSS', $id = 'gl_mooticker') { }
-}
-if ( !function_exists('WIDGET_moospring') ) {
-    function WIDGET_moospring() { }
-}
-if ( !function_exists('WIDGET_moorotator') ) {
-    function WIDGET_moorotator() { }
-}
-if ( !function_exists('WIDGETS_moowrapper') ) {
-    function WIDGET_wrapper() { }
 }
 ?>
