24bbce6dd2f00904972fd44c2853ebd7519e5c94
Mark R. Evans <mark@glfusion.org>
2014-06-13 13:05:49 -0500

    Move jquery to core JS space

---
+++ public_html/javascript/addons/tooltip.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('.tooltip').tooltipster({
+    contentAsHTML: true,
+    animation: 'fade',
+    delay: 100,
+    functionInit: function(origin, content) {
+      tipParts = content.split('::');
+      if ( tipParts[0] === '' ) {
+          return content;
+      }
+      tipTitle = tipParts[0];
+      tipData = tipParts[1];
+      formatted = '<span class="tool-title">' + tipTitle + '</span><br/>' + tipData;
+      return formatted;
+    }
+  });
+});
