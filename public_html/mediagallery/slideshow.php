fef029977a9094c4833f7b4b5b502fc98a85e24a
Mark R. Evans <mark@glfusion.org>
2015-07-25 21:52:44 -0500

    Tweak slide show

--- public_html/mediagallery/slideshow.php
+++ public_html/mediagallery/slideshow.php
@@ -49,22 +49,21 @@ MG_initAlbums();
 /*
 * Main Function
 */
-
 COM_setArgNames(array('aid','f','sort'));
 $album_id    = COM_applyFilter(COM_getArgument('aid'),true);
 $full        = COM_applyFilter(COM_getArgument('f'),true);
 $sortOrder   = COM_applyFilter(COM_getArgument('sort'),true);
 
-$themeStyle = MG_getThemeCSS($album_id);
-
 $T = new Template( MG_getTemplatePath($album_id) );
-$T->set_file (array(
-    'page'  =>  'slideshow.thtml',
-    'empty' =>  'slideshow_empty.thtml'
-));
+
+$T->set_file('page', 'slideshow.thtml');
+
 $T->set_var('header', $LANG_MG00['plugin']);
 $T->set_var('site_url',$_MG_CONF['site_url']);
-$T->set_var('plugin','mediagallery');
+$T->set_block('page', 'slideItems', 'sItems');
+$T->set_block('page', 'noItems', 'nItems');
+$T->set_var('header', $LANG_MG00['plugin']);
+$T->set_var('site_url', $_MG_CONF['site_url']);
 
 if ($MG_albums[$album_id]->access == 0 ) {
     $display = MG_siteHeader();
@@ -73,7 +72,6 @@ if ($MG_albums[$album_id]->access == 0 ) {
     echo $display;
     exit;
 }
-
 $album_title  = $MG_albums[$album_id]->title;
 $album_desc   = $MG_albums[$album_id]->description;
 $album_parent = $MG_albums[$album_id]->parent;
@@ -91,34 +89,30 @@ $sql = "SELECT * FROM " .
         $orderBy;
 
 $result = DB_query( $sql );
+
 $total_media = 0;
 $mediaObject = array();
-while ( $row = DB_fetchArray( $result ) ) {
-    if ($row['media_type'] != 0 || $row['media_filename'] == '')
-        continue;
+while ($row = DB_fetchArray($result)) {
+    if ($row['media_type'] != 0 || $row['media_filename'] == '') continue;
     $mediaObject[] = $row;
     $total_media++;
 }
 $noFullOption = 0;
-
-if ( $MG_albums[$album_id]->full == 2 || $_MG_CONF['discard_original'] == 1 || ( $MG_albums[$album_id]->full == 1 && (COM_isAnonUser() ))) {
+if ($album_data['full_display'] == 2 || $_MG_CONF['discard_original'] == 1 ||
+    ($album_data['full_display'] == 1 && (!isset($_USER['uid']) || $_USER['uid'] < 2 ))) {
     $full = 0;
     $noFullOption = 1;
 }
-
 $photoCount = 0;
 
-// default settings ---
-
-if ( $total_media > 0 ) {
+if ($total_media > 0) {
     $defaultLoop        = 0;
     $defaultTransition  = 0;
     $defaultPause       = 3;
     $defaultFull        = 0;
-
-    $y = 1;
-    $T->set_block('page', 'photo_url','purl');
-
+    $y                  = 1;
+    $maxheight          = 0;
+    $T->set_block('page', 'photo_url', 'purl');
     for ( $i=0; $i<$total_media; $i++ ) {
 
         if ( $full == 1 ) {
@@ -147,60 +141,40 @@ if ( $total_media > 0 ) {
             }
         }
 
+        if ( $imgsize[1] > $maxheight ) $maxheight = $imgsize[1];
+
         $PhotoCaption = $mediaObject[$i]['media_title'];
         $PhotoCaption = str_replace(";", " ",  $PhotoCaption);
         $PhotoCaption = str_replace("\"", " ", $PhotoCaption);
         $PhotoCaption = str_replace("\n", " ", $PhotoCaption);
         $PhotoCaption = str_replace("\r", " ", $PhotoCaption);
 
-        $T->set_var( array(
-            'URL'       => 'photo_urls[' . $y . '] = "' . $PhotoURL . '";',
-            'CAPTION'   => 'photo_captions[' . $y . '] = "' . $PhotoCaption . '";',
+        $T->set_var(array(
+            'URL'     => 'photo_urls[' . $y . '] = "' . $PhotoURL . '";',
+            'CAPTION' => 'photo_captions[' . $y . '] = "' . $PhotoCaption . '";',
         ));
-        $T->parse('purl', 'photo_url',true);
+        $T->parse('photo_info', 'photo_url', true);
         $y++;
         $photoCount++;
     }
-
-    $T->set_var(array(
-        'photo_count'   => $total_media,
-    ));
+    $T->set_var('photo_count', $total_media);
 } else {
-    $T->set_var(array(
-        'no_images' => '<br />' . $LANG_MG03['no_media_objects'],
-    ));
-
+    $T->set_var('no_images', '<br>' . $LANG_MG03['no_media_objects']);
 }
-
-if ($noFullOption == 0 ) {
-    $full_toggle =  '&nbsp;<a href="' .
-                    $_MG_CONF['site_url'] .
-                    '/slideshow.php?aid=' .
-                    $album_id .
-                    '&amp;f=' .
-                    ($full ? '0' : '1') .
-                    '&amp;sort=' .
-                    $sortOrder .
-                    '">[' .
-                    ($full ? $LANG_MG03['normal_size'] : $LANG_MG03['full_size']) .
-                    ']</a>';
-} else {
-    $full_toggle = '';
+$full_toggle = '';
+if ($noFullOption == 0) {
+    $full_toggle = '<a href="' . $_MG_CONF['site_url'] . '/slideshow.php?aid=' . $album_id . '&amp;f=' . ($full ? '0' : '1') .
+                   '&amp;sort=' . $sortOrder . '">' . ($full ? $LANG_MG03['normal_size'] : $LANG_MG03['full_size']) . '</a>';
 }
-
-
 $T->set_var(array(
-    'birdseed'          => $birdseed,
-    'pagination'        => '<a href="' . $_MG_CONF['site_url'] . '/album.php?aid=' . $album_id . '&amp;page=1&amp;sort=' . $sortOrder . '">' . $LANG_MG03['return_to_album'] .'</a>',
-    'slideshow'         => $_MG_CONF['site_url'] . '/slideshow.php?aid=' . $album_id . '&amp;f=' . ($full ? '0' : '1') . '&amp;sort=' . $sortOrder ,
-    'slideshow_size'    => ($full ? $LANG_MG03['normal_size'] : $LANG_MG03['full_size']),
-    'full_toggle'       => $full_toggle,
-    'album_title'       => $album_title,
-));
-
-// Set language items...
-
-$T->set_var(array(
+    'maxheight'             => $maxheight + 125,
+    'pagination'            => '<a href="' . $_MG_CONF['site_url'] . '/album.php?aid=' . $album_id . '&amp;page=1&amp;sort=' . $sortOrder . '">' . $LANG_MG03['return_to_album'] .'</a>',
+    'slideshow'             => $_MG_CONF['site_url'] . '/slideshow.php?aid=' . $album_id . '&amp;f=' . ($full ? '0' : '1') . '&amp;sort=' . $sortOrder ,
+    'slideshow_size'        => ($full ? $LANG_MG03['normal_size'] : $LANG_MG03['full_size']),
+    'full_toggle'           => $full_toggle,
+    'album_title'           => $album_title,
+    'max_image_height'      => $dImageHeight,
+    'max_image_width'       => $dImageWidth,
     'home'                  => $LANG_MG03['home'],
     'return_to_album'       => $LANG_MG03['return_to_album'],
     'normal_size'           => $LANG_MG03['normal_size'],
@@ -213,35 +187,18 @@ $T->set_var(array(
     'forward'               => $LANG_MG03['forward'],
     'picture_loading'       => $LANG_MG03['picture_loading'],
     'please_wait'           => $LANG_MG03['please_wait'],
-    'transition'            => $LANG_MG03['transition'],
     'delay'                 => $LANG_MG03['delay'],
     'loop'                  => $LANG_MG03['loop'],
     'seconds'               => $LANG_MG03['seconds'],
     'lang_of'               => $LANG_MG03['of'],
-    'lang_blend'            => $LANG_MG05['blend'],
-    'lang_blinds'           => $LANG_MG05['blinds'],
-    'lang_checkerboard'     => $LANG_MG05['checkerboard'],
-    'lang_diagonal'         => $LANG_MG05['diagonal'],
-    'lang_doors'            => $LANG_MG05['doors'],
-    'lang_gradient'         => $LANG_MG05['gradient'],
-    'lang_iris'             => $LANG_MG05['iris'],
-    'lang_pinwheel'         => $LANG_MG05['pinwheel'],
-    'lang_pixelate'         => $LANG_MG05['pixelate'],
-    'lang_radial'           => $LANG_MG05['radial'],
-    'lang_rain'             => $LANG_MG05['rain'],
-    'lang_slide'            => $LANG_MG05['slide'],
-    'lang_snow'             => $LANG_MG05['snow'],
-    'lang_spiral'           => $LANG_MG05['spiral'],
-    'lang_stretch'          => $LANG_MG05['stretch'],
-    'lang_random'           => $LANG_MG05['random']
-
 ));
 
-if ( $total_media > 0 ) {
-    $T->parse('output','page');
+if ($total_media > 0) {
+    $T->parse('sItems', 'slideItems');
 } else {
-    $T->parse('output','empty');
+    $T->parse('nItems', 'noItems');
 }
+$T->parse('output','page');
 $display = MG_siteHeader(strip_tags($MG_albums[$album_id]->title));
 $display .= $T->finish($T->get_var('output'));
 $display .= MG_siteFooter();
