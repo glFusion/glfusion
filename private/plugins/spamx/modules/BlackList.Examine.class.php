87bbf36d20f826b554e0b536c517438a96492a33
Mark R. Evans <mark@glfusion.org>
2015-10-23 21:31:05 -0500

    preg update

--- private/plugins/spamx/modules/BlackList.Examine.class.php
+++ private/plugins/spamx/modules/BlackList.Examine.class.php
@@ -29,15 +29,23 @@ require_once $_CONF['path'] . 'plugins/spamx/modules/' . 'BaseCommand.class.php'
  * @package Spam-X
  *
  */
+class BlackList extends BaseCommand
+{
+    // Callback functions for preg_replace_callback()
+    protected function callbackDecimal($str)
+    {
+        return chr($str);
+    }
 
-class BlackList extends BaseCommand {
-    /**
-     * No Constructor Use BaseCommand constructor
-     */
+    protected function callbackHex($str)
+    {
+        return chr('0x' . $str);
+    }
+
     /**
      * Here we do the work
      */
-    function execute ($comment)
+    public function execute($comment)
     {
         global $_CONF, $_TABLES, $_USER, $LANG_SX00;
 
@@ -50,27 +58,31 @@ class BlackList extends BaseCommand {
         /**
          * Include Blacklist Data
          */
-        $result = DB_query ("SELECT value FROM {$_TABLES['spamx']} WHERE name='Personal'", 1);
-        $nrows = DB_numRows ($result);
+        $result = DB_query("SELECT value FROM {$_TABLES['spamx']} WHERE name='Personal'", 1);
+        $nrows = DB_numRows($result);
 
         // named entities
-        $comment = html_entity_decode ($comment);
+        $comment = html_entity_decode($comment);
         // decimal notation
-        $comment = preg_replace ('/&#(\d+);/me', "chr(\\1)", $comment);
+        $comment = preg_replace_callback('/&#(\d+);/m', array($this, 'callbackDecimal'), $comment);
         // hex notation
-        $comment = preg_replace ('/&#x([a-f0-9]+);/mei', "chr(0x\\1)", $comment);
-        $ans = 0;
+        $comment = preg_replace_callback('/&#x([a-f0-9]+);/mi', array($this, 'callbackHex'), $comment);
+         $ans = 0;
         for ($i = 1; $i <= $nrows; $i++) {
             list ($val) = DB_fetchArray ($result);
+
             $val = str_replace ('#', '\\#', $val);
+
             if (preg_match ("#$val#i", $comment)) {
-                $ans = 1; // quit on first positive match
+                $ans = 1;  // quit on first positive match
+
                 SPAMX_log ($LANG_SX00['foundspam'] . $val .
                            $LANG_SX00['foundspam2'] . $uid .
                            $LANG_SX00['foundspam3'] . $_SERVER['REMOTE_ADDR']);
                 break;
             }
         }
+
         return $ans;
     }
 }
