b844c21cccd09d581af21f86e7c915549135d6f1
Mark R. Evans <mark@glfusion.org>
2015-11-06 13:28:04 -0600

    Bump version on install module

--- public_html/admin/install/index.php
+++ public_html/admin/install/index.php
@@ -35,7 +35,7 @@
 error_reporting( E_ERROR | E_WARNING | E_PARSE | E_COMPILE_ERROR );
 @ini_set('opcache.enable','0');
 if (!defined('GVERSION')) {
-    define('GVERSION', '1.5.0');
+    define('GVERSION', '1.5.1');
 }
 
 define('SESSION_EXPIRED',           1);
