cea0bac34e7b1866c914b33614850b6f5178bd69
Mark R. Evans <mark@glfusion.org>
2015-11-26 22:52:12 -0600

    General language updates

--- public_html/admin/envcheck.php
+++ public_html/admin/envcheck.php
@@ -92,7 +92,7 @@ function _checkEnvironment()
     } else {
         $T->set_var('status','<span class="yes">'.phpversion().'</span>');
     }
-    $T->set_var('recommended','5.2.0+');
+    $T->set_var('recommended','5.3.0+');
     $T->set_var('notes',$LANG01['php_req_version']);
     $T->set_var('rowclass',($classCounter % 2)+1);
     $T->parse('env','envs',true);
