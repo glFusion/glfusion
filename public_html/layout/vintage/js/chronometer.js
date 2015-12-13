c1ed9683ff4e6f6bbfbcdaa0eaba161ff8407b6d
Mark R. Evans <mark@glfusion.org>
2015-12-13 14:36:33 -0600

    Chronometer fix

--- public_html/layout/vintage/js/chronometer.js
+++ public_html/layout/vintage/js/chronometer.js
@@ -1,34 +1,31 @@
-function applyclass()
-{
-    chronometer=""  // "" for auto-rotation | "day" for static day | "night" for static night, etc.
-    if (document.body.id == '') {
-    	if (chronometer != false) {
-    		timeofday = chronometer;
-    		document.body.id=chronometer;
-    	} else {
-            var d = new Date();
-            var thehour = d.getHours();
-    		if (thehour >= 20){
-    			timeofday = "night";
-    		}
-    		else if (thehour >= 18) {
-    			timeofday = "dusk";
-    		}
-    		else if (thehour >= 15){
-    			timeofday = "afternoon";
-    		}
-    		else if (thehour >= 9){
-    			timeofday = "day";
-    		}
-    		else if (thehour >= 6){
-    			timeofday = "dawn";
-    		}
-    		else {
-    			timeofday = "night";
-    		}
-    		document.body.id=timeofday;
-    	}
-    	Cookie.set('chronometer', timeofday, {duration: 1/24,path: "/"});
-    }
-}
-window.onload = applyclass;
+$(window).ready(function() {
+	chronometer=""  // "" for auto-rotation | "day" for static day | "night" for static night, etc.
+	if (document.body.id == '') {
+		if (chronometer != false) {
+			timeofday = chronometer;
+			document.body.id=chronometer;
+		} else {
+			var d = new Date();
+			var thehour = d.getHours();
+			if (thehour >= 20){
+				timeofday = "night";
+			}
+			else if (thehour >= 18) {
+				timeofday = "dusk";
+			}
+			else if (thehour >= 15){
+				timeofday = "afternoon";
+			}
+			else if (thehour >= 9){
+				timeofday = "day";
+			}
+			else if (thehour >= 6){
+				timeofday = "dawn";
+			}
+			else {
+				timeofday = "night";
+			}
+			document.body.id=timeofday;
+		}
+	}
+});
