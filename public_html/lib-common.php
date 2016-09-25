7bb6e7a50eb12838b6fb311c55a2391baa50c170
Mark R. Evans <mark@glfusion.org>
2016-09-24 21:46:58 -0500

    Fixed incorrect language reference

--- public_html/lib-common.php
+++ public_html/lib-common.php
@@ -1287,7 +1287,7 @@ function COM_siteFooter( $rightblock = -1, $custom = '' )
     $theme->set_var( 'current_year', $year );
     $theme->set_var( 'lang_copyright', $LANG01[93] );
     $theme->set_var( 'trademark_msg', $LANG01[94] );
-    $theme->set_var( 'powered_by', $LANG01[95]);
+    $theme->set_var( 'powered_by', $LANG01[93]);
     $theme->set_var( 'glfusion_url', 'http://www.glfusion.org/' );
     $theme->set_var( 'glfusion_version', GVERSION );
     $theme->set_var( 'direction',(empty($LANG_DIRECTION) ? 'ltr' : $LANG_DIRECTION));
