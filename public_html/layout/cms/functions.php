380daa165bc1a4d1e2af500b75342f621dd311c8
Mark R. Evans <mark@glfusion.org>
2016-11-22 08:36:54 -0600

    Update jquery-ui

--- public_html/layout/cms/functions.php
+++ public_html/layout/cms/functions.php
@@ -106,6 +106,11 @@ $blockInterface = array(
 
 // define the JS we need for this theme..
 $outputHandle = outputHandler::getInstance();
+
+// if the theme needs jquery-ui - uncomment
+// also see jquery-ui styles below..
+//$outputHandle->addScriptFile($_CONF['path_html'].'javascript/jquery/jquery-ui.min.js');
+
 $outputHandle->addScriptFile($_CONF['path_html'].'javascript/ps.js');
 $outputHandle->addScriptFile($_CONF['path_layout'].'js/jquery.smartmenus.min.js');
 
@@ -166,8 +171,8 @@ $outputHandle->addScriptFile($_CONF['path_layout'].'js/components/upload.min.js'
 $outputHandle->addScriptFile($_CONF['path_html'].'javascript/addons/mediaplayer/mediaelement-and-player.min.js');
 $outputHandle->addCSSFile($_CONF['path_html'] .'javascript/addons/mediaplayer/mediaelementplayer.css');
 
-// must load the jquery ui library we want to use.
-$outputHandle->addLinkStyle($_CONF['layout_url'].'/css/ui-uikit/jquery-ui-1.10.4.custom.min.css');
+// must load the jquery ui library we want to use if using jquery ui.
+//$outputHandle->addLinkStyle($_CONF['layout_url'].'/css/jquery-ui/jquery-ui.min.css');
 
 //  Custom CSS
 if ( file_exists($_CONF['path_layout'] .'custom.css') ) {
