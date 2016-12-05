076ec190a37d7c655397b2cefa77fc2e930ebacd
matrox66 <matrox66@gmx.net>
2016-12-05 12:09:47 +0100

    German Update staticpages

--- private/plugins/staticpages/language/german_utf-8.php
+++ private/plugins/staticpages/language/german_utf-8.php
@@ -3,6 +3,7 @@
 # german_utf-8.php
 #
 # This is the German language file for the glFusion Static Pages plugin
+# addressing the user as "Du" (informal German).
 #
 # Copyright (C) 2001 Tony Bibbs
 # tony AT tonybibbs DOT com
@@ -10,6 +11,7 @@
 # German translation by Dirk Haun <dirk AT haun-online DOT de>
 # and Markus Wollschläger
 # Modifiziert: August 09 Tony Kluever
+# Siegfried Gutschi (November 2016) <sigi AT modellbaukalender DOT info>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -55,40 +57,40 @@ $LANG_STATIC = array(
     'edit' => 'Bearbeiten',
     'lastupdated' => 'Letzte Änderung',
     'pageformat' => 'Seitenformat',
-    'leftrightblocks' => 'Blöcke links &amp; rechts',
+    'leftrightblocks' => 'Linke &amp; Rechte Blöcke',
     'blankpage' => 'Leere Seite',
     'noblocks' => 'Keine Blöcke',
-    'leftblocks' => 'Blöcke links',
-    'rightblocks' => 'Right Blocks',
+    'leftblocks' => 'Linke Blöcke',
+    'rightblocks' => 'Rechte Blöcke',
     'addtomenu' => 'Ins Menü aufnehmen',
     'label' => 'Label',
     'nopages' => 'Es sind keine statischen Seiten vorhanden.',
     'save' => 'Speichern',
     'preview' => 'Vorschau',
     'delete' => 'Löschen',
-    'cancel' => 'Abbruch',
+    'cancel' => 'Abbrechen',
     'access_denied' => 'Zugriff verweigert',
-    'access_denied_msg' => 'Unerlaubter Zugriff, auf eine der Admin-Seiten für Statische Seiten. Hinweis: Alle derartigen Versuche werden protokolliert',
+    'access_denied_msg' => 'Du besitzt nicht die nötigen Berechtigungen, um auf diese Seite zugreifen zu können.  Dein Benutzername und Deine IP wurden aufgezeichnet.',
     'all_html_allowed' => 'Alle HTML-Tags sind erlaubt',
     'results' => 'Gefundene Statische Seiten',
     'author' => 'Autor',
     'no_title_or_content' => 'Bitte mindestens die Felder <b>Titel</b> und <b>Inhalt</b> ausfüllen.',
     'no_such_page_anon' => 'Bitte anmelden.',
-    'no_page_access_msg' => "Dies könnte passiert sein, weil Du nicht angemeldet bist, oder kein Mitglied bist von  {$_CONF['site_name']}. Bitte <a href=\"{$_CONF['site_url']}/users.php?mode=new\"> Mitglied werden</a> bei {$_CONF['site_name']}, um vollen Zugriff zu erhalten.",
+    'no_page_access_msg' => "Dies könnte passiert sein, weil Du nicht angemeldet bist, oder kein Mitglied von {$_CONF['site_name']} bist. Bitte <a href=\"{$_CONF['site_url']}/users.php?mode=new\">Registrieren</a> bei {$_CONF['site_name']}, um vollen Zugriff zu erhalten.",
     'php_msg' => 'PHP: ',
     'php_warn' => 'Hinweis: Wenn diese Option aktiviert ist, wird in der Seite enthaltener PHP-Code ausgeführt. <em>Bitte mit Bedacht verwenden!</em>',
     'exit_msg' => 'Hinweistext: ',
     'exit_info' => 'Art des Hinweistextes, wenn kein Zugriff auf die Seite erlaubt ist: Aktiviert = "Anmeldung erforderlich", nicht aktiviert = "Zugriff verweigert".',
     'deny_msg' => 'Zugriff auf diese Seite ist nicht möglich. Die Seite wurde entweder umbenannt oder gelöscht oder Du hast nicht die nötigen Zugriffsrechte.',
-    'stats_headline' => 'Top Ten der Statischen Seiten',
+    'stats_headline' => 'Top 10 der Statischen Seiten',
     'stats_page_title' => 'Titel',
     'stats_hits' => 'Aufrufe',
-    'stats_no_hits' => 'Es gibt keine Statischen Seiten oder sie wurden von niemandem gelesen.',
+    'stats_no_hits' => 'Es wurden noch keine Statischen Seiten angelegt.',
     'id' => 'ID',
     'duplicate_id' => 'Diese ID wird bereits für eine andere Statische Seite benutzt. Bitte andere ID wählen.',
-    'instructions' => 'Um eine Statische Seite zu ändern oder zu löschen, auf das Ändern-Icon klicken. Um eine Statische Seite anzusehen, auf deren Titel klicken. Auf Neu anlegen (s.o.) klicken, um einen neue Statische Seite anzulegen. Auf das Kopie-Icon klicken, um eine Kopie einer vorhandenen Seite zu erhalten.',
-    'centerblock' => 'Centerblock: ',
-    'centerblock_msg' => 'Wenn angekreuzt wird diese Seite als Block auf der Index-Seite angezeigt.',
+    'instructions' => '<ul><li>Um eine Statische Seite zu ändern oder zu löschen, bitte auf das Bearbeiten-Icon klicken.</li><li>Um eine Statische Seite anzusehen, auf deren Titel klicken.</li><li>Auf "Neu anlegen" klicken, um einen neue Statische Seite anzulegen.</li><li>Auf das Kopie-Icon klicken, um eine Kopie einer vorhandenen Seite zu erhalten.</li></ul>',
+    'centerblock' => 'Mittel-Block',
+    'centerblock_msg' => 'Wenn angekreuzt wird diese Statische-Seite als Mittel-Block angezeigt.',
     'topic' => 'Kategorie: ',
     'position' => 'Position: ',
     'all_topics' => 'Alle',
@@ -97,14 +99,14 @@ $LANG_STATIC = array(
     'position_feat' => 'Nach Hauptartikel',
     'position_bottom' => 'Seitenende',
     'position_entire' => 'Ganze Seite',
-    'position_nonews' => 'Only if No Other News',
-    'head_centerblock' => 'Centerblock',
+    'position_nonews' => 'Nur wenn keine anderen Artikel',
+    'head_centerblock' => 'Mittel-Block',
     'centerblock_no' => 'Nein',
-    'centerblock_top' => 'oben',
+    'centerblock_top' => 'Oben',
     'centerblock_feat' => 'Hauptartikel',
-    'centerblock_bottom' => 'unten',
-    'centerblock_entire' => 'Ganze Seite',
-    'centerblock_nonews' => 'If No News',
+    'centerblock_bottom' => 'Unten',
+    'centerblock_entire' => 'Als ganze Seite',
+    'centerblock_nonews' => 'Wenn keine Artikel',
     'inblock_msg' => 'Im Block: ',
     'inblock_info' => 'Block-Templates für diese Seite verwenden.',
     'title_edit' => 'Seite ändern',
@@ -117,22 +119,22 @@ $LANG_STATIC = array(
     'printable_format' => 'Druckfähige Version',
     'copy' => 'Kopieren',
     'limit_results' => 'Ergebnisse einschränken',
-    'search' => 'Suchen',
+    'search' => 'In Suche aufnehmen',
     'submit' => 'Absenden',
     'delete_confirm' => 'Möchtest Du diese Seite wirklich löschen?',
-    'allnhp_topics' => 'All Topics (No Homepage)',
-    'page_list' => 'Page List',
-    'instructions_edit' => 'This screen allows you to create / edit a new static page. Pages can contain PHP code and HTML code.',
-    'attributes' => 'Attributes',
-    'preview_help' => 'Select the <b>Preview</b> button to refresh the preview display',
-    'page_saved' => 'Page has been successfully saved.'
+    'allnhp_topics' => 'Alle, aber nicht Startseite',
+    'page_list' => 'Seiten-Übersicht',
+    'instructions_edit' => '<ul><li>Hier kannst Du eine neue Statische-Seite erstellen oder vorhandene Seiten bearbeiten.</li><li>Seiten können PHP-Code und HTML-Code enthalten.</li></ul>',
+    'attributes' => 'Allgemeines',
+    'preview_help' => 'Wählen Sie die Schaltfläche <b>Vorschau</b>, um die Vorschauanzeige zu aktualisieren',
+    'page_saved' => 'Seite wurde erfolgreich gespeichert.'
 );
 ###############################################################################
 # autotag descriptions
 
 $LANG_SP_AUTOTAG = array(
-    'desc_staticpage' => 'Link: to a staticpage on this site; link_text defaults to staticpage title. usage: [staticpage:<i>page_id</i> {link_text}]',
-    'desc_staticpage_content' => 'HTML: renders the content of a staticpage.  usage: [staticpage_content:<i>page_id</i>]'
+    'desc_staticpage' => 'Link: zu einer Statischen-Seite; link_text defaults to staticpage title. usage: [staticpage:<i>page_id</i> {link_text}]',
+    'desc_staticpage_content' => 'HTML: zeigt den Inhalt einer Statischen.-Seite.  usage: [staticpage_content:<i>page_id</i>]'
 );
 
 
@@ -140,31 +142,31 @@ $PLG_staticpages_MESSAGE19 = '';
 $PLG_staticpages_MESSAGE20 = '';
 
 // Messages for the plugin upgrade
-$PLG_staticpages_MESSAGE3001 = 'Plugin-Upgrade nicht unterstützt.';
+$PLG_staticpages_MESSAGE3001 = 'Static-Pages-Plugin Aktualisierung: Diese Version kann nicht automatisch aktualisiert werden.';
 $PLG_staticpages_MESSAGE3002 = $LANG32[9];
 
 // Localization of the Admin Configuration UI
 $LANG_configsections['staticpages'] = array(
-    'label' => 'Statische Seiten',
-    'title' => 'Konfiguration - Statische Seiten'
+    'label' => 'Statische-Seiten',
+    'title' => 'Statische-Seiten-Konfig.'
 );
 
 $LANG_confignames['staticpages'] = array(
     'allow_php' => 'PHP erlauben?',
-    'sort_by' => 'Centerblocks sortieren nach',
-    'sort_menu_by' => 'Menüeinträge sortieren nach',
-    'delete_pages' => 'Seiten mit Benutzer löschen?',
-    'in_block' => 'Block-Template verwenden?',
-    'show_hits' => 'Aufrufe anzeigen?',
-    'show_date' => 'Datum anzeigen?',
-    'filter_html' => 'HTML filtern?',
-    'censor' => 'Inhalt zensieren?',
+    'sort_by' => 'Mittel-Blöcke sortieren nach',
+    'sort_menu_by' => 'Menü-Einträge sortieren nach',
+    'delete_pages' => 'Seiten mit Benutzer löschen',
+    'in_block' => 'Block-Template verwenden',
+    'show_hits' => 'Aufrufe anzeigen',
+    'show_date' => 'Datum anzeigen',
+    'filter_html' => 'HTML filtern',
+    'censor' => 'Inhalt zensieren',
     'default_permissions' => 'Standardeinstellungen - Statische Seiten',
-    'aftersave' => 'Nach dem Speichern der Seiten',
-    'atom_max_items' => 'Max. Seiten in Webservices News-Feed',
-    'comment_code' => 'Comment Default',
-    'include_search' => 'Site Search Default',
-    'status_flag' => 'Default Page Mode'
+    'aftersave' => 'Nach dem Speichern',
+    'atom_max_items' => 'Max. Seiten im News-Feed',
+    'comment_code' => 'Kommentar-Standard',
+    'include_search' => 'In Suche aufnehmen-Standart',
+    'status_flag' => 'Seite Sichtbar-Standart'
 );
 
 $LANG_configsubgroups['staticpages'] = array(
@@ -172,8 +174,8 @@ $LANG_configsubgroups['staticpages'] = array(
 );
 
 $LANG_fs['staticpages'] = array(
-    'fs_main' => 'Statische Seiten - Haupteinstellungen',
-    'fs_permissions' => 'Standardberechtigungen - Statische Seiten'
+    'fs_main' => 'Allgemeine-Einstellungen',
+    'fs_permissions' => 'Standard-Berechtigungen'
 );
 
 // Note: entries 0, 1, 9, and 12 are the same as in $LANG_configselects['Core']
@@ -184,8 +186,8 @@ $LANG_configselects['staticpages'] = array(
     3 => array('Datum' => 'date', 'Seiten-ID' => 'id', 'Titel' => 'title', 'Menüpunkt' => 'label'),
     9 => array('Zur Seite weiterleiten' => 'item', 'Liste anzeigen' => 'list', 'Startseite' => 'home', 'Kommandozentrale' => 'admin'),
     12 => array('Kein Zugang' => 0, 'Nur lesen' => 2, 'Lesen-Schreiben' => 3),
-    13 => array('Enabled' => 1, 'Disabled' => 0),
-    17 => array('Comments Enabled' => 0, 'Comments Disabled' => -1)
+    13 => array('Aktiviert' => 1, 'Deaktiviert' => 0),
+    17 => array('Kommentare erlaubt' => 0, 'Kommentare nicht erlaubt' => -1)
 );
 
 ?>
