ef566f497812569fccc09dd44e118db01448994b
Mark R. Evans <mark@glfusion.org>
2017-09-04 20:31:38 -0500

    Remove pear

--- private/vendor/composer/autoload_real.php
+++ private/vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInit9b80ed771292a3b6ed07027fef7a1468
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit9b80ed771292a3b6ed07027fef7a1468', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
