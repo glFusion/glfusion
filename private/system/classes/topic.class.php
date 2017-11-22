5d3aad467ffff4d374db0efde8e34ae9db328dd5
Lee Garner <lee@leegarner.com>
2017-11-22 14:25:08 -0800

    Isolate reads to constructor (#280)
    * Makes better sense to load original TID before saving
    
    * Separate Read from Get to avoid duplicate instantiation
    
    * Change form var for topic name to avoid conflict with lib-common setting current topic
    
    * Isolate DB reads to constructor

--- private/system/classes/topic.class.php
+++ private/system/classes/topic.class.php
@@ -238,8 +238,8 @@ class Topic
 
     /**
     *   Constructor.
-    *   Reads the topic into the object variables.
-    *   Does not set the current topic.
+    *   Gets the object properties from cache if found, otherwise
+    *   reads data from the DB and caches the new object.
     *
     *   @param  mixed   $tid    Topic ID, or complete topic record from DB
     */
@@ -258,12 +258,17 @@ class Topic
                 $this->setVars($tid, true);
             } else {
                 // A single topic ID passed in as a string
-                // Gets the info from self::Get() to ensure properties array
+                // Gets the info from self::Read() to ensure properties array
                 // is populated.
-                $obj = self::Get($tid);
-                if ($obj) {
+                if (isset(self::$cache[$tid])) {
+                    $props = self::$cache[$tid]->properties;
+                } else {
+                    $props = self::Read($tid);
+                }
+                if (!empty($props)) {
+                    $this->setVars($props, true);
                     $this->isNew = false;
-                    $this->setVars($obj->properties, true);
+                    self::$cache[$tid] = $this;
                 }
             }
         }
@@ -287,7 +292,7 @@ class Topic
         } else {
             $this->old_tid  = $A['old_tid'];
             $this->tid      = $A['tid'];
-            $this->topic    = $A['topic'];
+            $this->topic    = $A['topic_name'];
             $this->description = $A['description'];
             $this->imageurl = $A['imageurl'];
             $this->sortnum  = $A['sortnum'];
@@ -360,7 +365,34 @@ class Topic
 
 
     /**
-    *   Wrapper to get a value and return a default for empty topics.
+    *   Read a single topic from the database
+    *
+    *   @param  string  $tid    Topic ID
+    *   @param  mixed       Array of values, or NULL if not found
+    */
+    private static function Read($tid)
+    {
+        global $_TABLES;
+
+        $tid = COM_sanitizeID($tid, false);
+        if (empty($tid)) return NULL;
+
+        $sql = "SELECT * FROM {$_TABLES['topics']} WHERE tid = '$tid'";
+        $res = DB_query($sql, 1);
+        if (DB_error()) {
+            COM_errorLog(__CLASS__ . '::' . __FUNCTION__ . ': Unable to read topics table');
+            return NULL;
+        } elseif (DB_numRows($res) == 0) {
+            COM_errorLog(__CLASS__ . '::' . __FUNCTION__ . ": Topic $tid not found");
+            return NULL;;
+        } else {
+            return DB_fetchArray($res, false);
+        }
+    }
+
+
+    /**
+    *   Wrapper to get an object or specific value
     *
     *   @param  string  $key        Name of value
     *   @param  mixed   $default    Default value, NULL to use class default
@@ -368,8 +400,6 @@ class Topic
     */
     public static function Get($tid, $key = NULL, $default = NULL)
     {
-        global $_TABLES;
-
         $obj = NULL;
         $tid = COM_sanitizeID($tid, false);
         if (empty($tid)) return NULL;
@@ -377,22 +407,9 @@ class Topic
         // Get the topic object, first checking cache then the DB
         if (isset(self::$cache[$tid])) {
             $obj = self::$cache[$tid];
-        } else {    // Attempt to read the topic
-            $sql = "SELECT * FROM {$_TABLES['topics']} WHERE tid = '$tid'";
-            $res = DB_query($sql, 1);
-            if (DB_error()) {
-                COM_errorLog(__CLASS__ . '::' . __FUNCTION__ . ': Unable to read topics table');
-                return NULL;
-            } elseif (DB_numRows($res) == 0) {
-                COM_errorLog(__CLASS__ . '::' . __FUNCTION__ . ": Topic $tid not found");
-                return NULL;;
-            } else {
-                $A = DB_fetchArray($res, false);
-                self::$cache[$A['tid']] = new self($A);
-                $obj = self::$cache[$A['tid']];
-            }
+        } else {
+            $obj = new self($tid);
         }
-
         if ($obj === NULL) {
             if ($key !== NULL) {
                 // Bad topic, key requested, return default or NULL if none
