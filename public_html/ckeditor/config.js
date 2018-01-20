f1b9d8cdf5739420f0185f680e65dd94f2320a73
Mark R. Evans <mark@glfusion.org>
2018-01-20 07:28:52 -0600

    editor customizations

--- public_html/ckeditor/config.js
+++ public_html/ckeditor/config.js
@@ -7,7 +7,8 @@ var tbitems = {
 		'clipboard'     : ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord'],
 		'undo'          : ['Undo', 'Redo'],
 		'editing'       : ['Find', 'Replace', '-',  'Scayt'],
-		'links'         : ['Link', 'Unlink', 'Anchor'],
+		'links'         : ['Link', 'Unlink'],
+		'anchor'				: ['Anchor'],
 		'basicstyles'   : ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'],
 		'insert'        : ['Image', 'Mediagallery', 'Youtube2', 'Vimeo','Table', 'HorizontalRule', 'SpecialChar'],
 		'paragraph'     : ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
@@ -37,6 +38,7 @@ CKEDITOR.editorConfig = function( config ) {
 		tbitems.items['undo'],
 		tbitems.items['editing'],
 		tbitems.items['links'],
+		tbitems.items['anchor'],
 		tbitems.items['insert'],
 		tbitems.items['basicstyles'],
 		tbitems.items['paragraph'],
@@ -52,8 +54,20 @@ CKEDITOR.editorConfig = function( config ) {
 	config.extraPlugins = 'youtube2,vimeo';
 	config.entities_latin = false;
 	config.scayt_autoStartup = true;
+	config.allowedContent = {
+	    $1: {
+	        // Use the ability to specify elements as an object.
+	        elements: CKEDITOR.dtd,
+	        attributes: true,
+	        styles: true,
+	        classes: true
+	    }
+	};
+	config.disallowedContent = 'table[cellspacing,cellpadding,border]';
 
     // FileMan
 	config.filebrowserBrowseUrl =  site_url + '/ckeditor/plugins/fileman/index.html?type=file';
 	config.filebrowserImageBrowseUrl = site_url + '/ckeditor/plugins/fileman/index.html?type=image';
+
+	config.customConfig = site_url+'/ckeditor/custom_config.js';
 };
