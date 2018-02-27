094a91cd5cc65bda6b2019695ee43ab6a243c569
Mark R. Evans <mark@glfusion.org>
2018-02-27 14:10:51 -0600

    Force tag to string if numeric

--- private/system/classes/Cache.php
+++ private/system/classes/Cache.php
@@ -150,9 +150,9 @@ final class Cache
                 $cacheItem->expiresAfter(86400);
             }
             if (is_array($tag)) {
-
                 $nsTags = array_map(
                     function($tag) {
+                        if (!is_string($tag)) $tag = (string)$tag;
                         if ($this->namespace != '') $tag = $this->namespace.'_'.$tag;
                         return $tag;
                     },$tag);
