15010f26ec085030a89d73399608caf8ed43fe36
Mark R. Evans <mark@glfusion.org>
2018-04-20 16:28:06 -0500

    Improved attachment handling

--- public_html/forum/getattachment.php
+++ public_html/forum/getattachment.php
@@ -6,7 +6,7 @@
 // |                                                                          |
 // | AJAX Server functions for file attachments                               |
 // +--------------------------------------------------------------------------+
-// | Copyright (C) 2008-2013 by the following authors:                        |
+// | Copyright (C) 2008-2018 by the following authors:                        |
 // |                                                                          |
 // | Mark R. Evans          mark AT glfusion DOT org                          |
 // |                                                                          |
@@ -57,6 +57,31 @@ $filepath = $_FF_CONF['uploadpath'].'/'.$filename;
 
 if ( file_exists($filepath) ) {
     if ($fd = fopen ($filepath, "rb")) {
+
+        if(ini_get('zlib.output_compression')) {
+            ini_set('zlib.output_compression', 'Off');
+        }
+        ob_end_flush();
+        header('Pragma: public'); 	// required
+        header('Expires: 0');		// no cache
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Last-Modified: '.gmdate ('D, d M Y H:i:s', @filemtime ($filepath)).' GMT');
+        header('Cache-Control: private',false);
+        header('Content-Type: application/force-download');
+        header('Content-Disposition: attachment; filename="'.basename($realname).'"');
+        header('Content-Transfer-Encoding: binary');
+        if (!$_CONF['cookiesecure']) {
+            header('Pragma: no-cache');
+        }
+        header('Content-Length: '. @filesize($filepath) );
+        header('Connection: close');
+        ob_clean();
+        ob_end_flush();
+        flush();
+        fpassthru($fd);
+        flush();
+        die();
+/* --- old method
         header("Content-type: application/octet-stream");
         header("Content-Disposition: inline; filename=\"{$realname}\"");
         header('Expires: 0');
@@ -64,6 +89,7 @@ if ( file_exists($filepath) ) {
         header('Pragma: public');
         fpassthru($fd);
         fclose ($fd);
+*/
     } else {
         echo "Error: Cannot Display Selected File, $realname";
         COM_errorLog("Error: Cannot Display Selected File, $realname");
