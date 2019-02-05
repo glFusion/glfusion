bff298833b6153c24ead1375286c61123506d913
Mark R. Evans <mark@glfusion.org>
2019-02-05 14:26:11 -0600

    dbal conversion

--- public_html/rater.php
+++ public_html/rater.php
@@ -1,40 +1,44 @@
 <?php
-// +--------------------------------------------------------------------------+
-// | glFusion CMS                                                             |
-// +--------------------------------------------------------------------------+
-// | rater.php                                                                |
-// |                                                                          |
-// | Non AJAX rating script                                                   |
-// +--------------------------------------------------------------------------+
-// | Copyright (C) 2006-2018 by the following authors:                        |
-// |                                                                          |
-// | Mark R. Evans          mark AT glfusion DOT org                          |
-// +--------------------------------------------------------------------------+
-// | Copyright (C) 2006,2007,2008 by the following authors:                   |
-// |                                                                          |
-// | Authors:                                                                 |
-// | Ryan Masuga, masugadesign.com  - ryan@masugadesign.com                   |
-// | Masuga Design                                                            |
-// |http://masugadesign.com/the-lab/scripts/unobtrusive-ajax-star-rating-bar/ |
-// | Komodo Media (http://komodomedia.com)                                    |
-// | Climax Designs (http://slim.climaxdesigns.com/)                          |
-// | Ben Nolan (http://bennolan.com/behaviour/) for Behavio(u)r!              |
-// |                                                                          |
-// | Homepage for this script:                                                |
-// |http://www.masugadesign.com/the-lab/scripts/unobtrusive-ajax-star-rating-bar/
-// +--------------------------------------------------------------------------+
-// |                                                                          |
-// | Licensed under a Creative Commons Attribution 3.0 License.               |
-// | http://creativecommons.org/licenses/by/3.0/                              |
-// |                                                                          |
-// +--------------------------------------------------------------------------+
-
+/**
+* glFusion CMS
+*
+* glFusion Rating Interface
+*
+* @license Creative Commons Attribution 3.0 License.
+*     http://creativecommons.org/licenses/by/3.0/                              |
+*
+*  Copyright (C) 2008-2019 by the following authors:
+*   Mark R. Evans   mark AT glfusion DOT org
+*
+*  Based on original work Copyright (C) 2006,2007,2008 by the following authors:
+*   Ryan Masuga, masugadesign.com  - ryan@masugadesign.com
+*   Masuga Design
+*      http://masugadesign.com/the-lab/scripts/unobtrusive-ajax-star-rating-bar
+*   Komodo Media (http://komodomedia.com)
+*   Climax Designs (http://slim.climaxdesigns.com/)
+*   Ben Nolan (http://bennolan.com/behaviour/) for Behavio(u)r!
+*
+*  Homepage for this script:
+*  http://www.masugadesign.com/the-lab/scripts/unobtrusive-ajax-star-rating-bar/
+*
+*  This (Unobtusive) AJAX Rating Bar script is licensed under the
+*  Creative Commons Attribution 3.0 License
+*    http://creativecommons.org/licenses/by/3.0/
+*
+*  What that means is: Use these files however you want, but don't
+*  redistribute without the proper credits, please. I'd appreciate hearing
+*  from you if you're using this script.
+*
+*/
 
 require_once 'lib-common.php';
 
 if ( !isset($_CONF['rating_speedlimit']) ) {
     $_CONF['rating_speedlimit'] = 15;
 }
+
+use \glFusion\Database\Database;
+use \glFusion\Log\Log;
 
 //getting the values
 $vote_sent  = preg_replace("/[^0-9]/","",$_REQUEST['j']);
@@ -73,12 +77,21 @@ if ( $vote_sent < 1 || $vote_sent == 0) {
     exit;
 }
 
+$voted = 0;
+
+$db = Database::getInstance();
+
 // look up the item in our database....
 
-$sql = "SELECT * FROM {$_TABLES['rating']} WHERE type='".DB_escapeString($plugin)."' AND item_id='".DB_escapeString($id_sent)."'";
-$result = DB_query($sql);
-if ( DB_numRows($result) > 0 ) {
-    $row            = DB_fetchArray($result);
+$sql = "SELECT * FROM `{$_TABLES['rating']}`
+        WHERE type=? AND item_id=?";
+
+$row = $db->conn->fetchAssoc(
+            $sql,
+            array($plugin,$id_sent),
+            array(Database::STRING,Database::STRING)
+);
+if ($row !== false && $row !== null) {
     $count          = $row['votes'];
     $current_rating = $row['rating'];
     $rating_id      = $row['id'];
@@ -89,15 +102,22 @@ if ( DB_numRows($result) > 0 ) {
 }
 
 if ( $uid == 1 ) {
-    $sql = "SELECT id FROM {$_TABLES['rating_votes']} WHERE ip_address='".DB_escapeString($ip)."' AND item_id='".DB_escapeString($id_sent)."'";
+    $checkResult = $db->conn->fetchColumn(
+        "SELECT id FROM `{$_TABLES['rating_votes']}` WHERE ip_address=? AND item_id=?",
+        array($ip,$id_sent),
+        0,
+        array(Database::STRING,Database::STRING)
+    );
 } else {
-    $sql = "SELECT id FROM {$_TABLES['rating_votes']} WHERE (uid=$uid OR ip_address='".DB_escapeString($ip)."') AND item_id='".DB_escapeString($id_sent)."'";
+    $checkResult = $db->conn->fetchColumn(
+        "SELECT id FROM `{$_TABLES['rating_votes']}` WHERE (uid=? OR ip_address=?) AND item_id=?",
+        array($uid,$ip,$id_sent),
+        0,
+        array(Database::INTEGER,Database::STRING,Database::STRING)
+    );
 }
-$checkResult = DB_query($sql);
-if ( DB_numRows($checkResult) > 0 ) {
+if ($checkResult !== false && $checkResult !== null) {
     $voted = 1;
-} else {
-    $voted = 0;
 }
 
 COM_clearSpeedlimit($_CONF['rating_speedlimit'],'rate');
