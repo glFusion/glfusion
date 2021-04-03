5f41d21c83654bb201de0d905b8154564587140b
Mark <mark@glfusion.org>
2021-04-02 20:09:40 -0500

    PHP v8 Compatibility

--- public_html/admin/plugins/mediagallery/success.php
+++ public_html/admin/plugins/mediagallery/success.php
@@ -32,7 +32,7 @@ require_once $_MG_CONF['path_admin'] . 'navigation.php';
 
 function MG_return_bytes($val) {
     $val = trim($val);
-    $last = strtolower($val{strlen($val)-1});
+    $last = strtolower($val[strlen($val)-1]);
     switch($last) {
         // The 'G' modifier is available since PHP 5.1.0
         case 'g':
