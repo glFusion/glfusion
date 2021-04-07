2548f8eb6713f713d9798271c9a4155f4a535661
Mark <mark@glfusion.org>
2021-04-07 09:00:35 -0500

    Update header

--- private/system/classes/sanitizer.class.php
+++ private/system/classes/sanitizer.class.php
@@ -7,7 +7,7 @@
 * @license GNU General Public License version 2 or later
 *     http://www.opensource.org/licenses/gpl-license.php
 *
-*  Copyright (C) 2014-2019 by the following authors:
+*  Copyright (C) 2014-2021 by the following authors:
 *   Mark R. Evans   mark AT glfusion DOT org
 *
 */
