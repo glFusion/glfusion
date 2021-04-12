24dc99db3159ef7894e63da93ad75f8dd4474c1f
Mark <mark@glfusion.org>
2021-04-12 08:53:51 -0500

    Automatically create mediagallery storage directory if not exists.

--- private/plugins/mediagallery/include/lib-upload.php
+++ private/plugins/mediagallery/include/lib-upload.php
@@ -1,36 +1,24 @@
 <?php
-// +--------------------------------------------------------------------------+
-// | Media Gallery Plugin - glFusion CMS                                      |
-// +--------------------------------------------------------------------------+
-// | lib-upload.php                                                           |
-// |                                                                          |
-// | Upload library                                                           |
-// +--------------------------------------------------------------------------+
-// | Copyright (C) 2002-2018 by the following authors:                        |
-// |                                                                          |
-// | Mark R. Evans          mark AT glfusion DOT org                          |
-// +--------------------------------------------------------------------------+
-// |                                                                          |
-// | This program is free software; you can redistribute it and/or            |
-// | modify it under the terms of the GNU General Public License              |
-// | as published by the Free Software Foundation; either version 2           |
-// | of the License, or (at your option) any later version.                   |
-// |                                                                          |
-// | This program is distributed in the hope that it will be useful,          |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with this program; if not, write to the Free Software Foundation,  |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
-// |                                                                          |
-// +--------------------------------------------------------------------------+
+/**
+* glFusion CMS - Media Gallery Plugin
+*
+* Upload Library
+*
+* @license GNU General Public License version 2 or later
+*     http://www.opensource.org/licenses/gpl-license.php
+*
+*  Copyright (C) 2002-2021 by the following authors:
+*   Mark R. Evans   mark AT glfusion DOT org
+*
+*/
 
 // this file can't be used on its own
 if (!defined ('GVERSION')) {
     die ('This file can not be used on its own.');
 }
+
+use \glFusion\Log\Log;
+use \glFusion\FileSystem;
 
 require_once $_CONF['path'].'plugins/mediagallery/include/lib-exif.php';
 require_once $_CONF['path'].'plugins/mediagallery/include/lib-watermark.php';
@@ -493,6 +481,12 @@ function MG_getFile( $filename, $file, $albums, $caption = '', $description = ''
         $_USER['username'] = 'guestuser';
     }
 
+    if ( FileSystem::mkDir($_MG_CONF['tmp_path']) === false ) {
+        Log::write('system',Log::ERROR,'MediaGallery: Media Upload failed as _MG_CONF[tmp_path] does not exist');
+        $errMsg .= sprintf($LANG_MG02['move_error'],$filename);
+        return array(false,$errMsg);
+    }
+
     $tmpPath = $_MG_CONF['tmp_path'] . '/' . $_USER['username'] . COM_makesid() . '.tmp';
 
     if ($upload) {
@@ -846,6 +840,22 @@ function MG_getFile( $filename, $file, $albums, $caption = '', $description = ''
             $media_time = getOriginationTimestamp($filename);
             if ( $media_time == null || $media_time < 0 ) {
                 $media_time = time();
+            }
+
+            if ( FileSystem::mkDir($_MG_CONF['path_mediaobjects'] . 'orig/' . $media_filename[0] . '/') === false ) {
+                Log::write('system',Log::ERROR,'MediaGallery: Unable to create directory: ' . $_MG_CONF['path_mediaobjects'] . 'orig/' . $media_filename[0] . '/');
+                $errMsg .= sprintf($LANG_MG02['move_error'],$filename);
+                return array(false,$errMsg);
+            }
+            if ( FileSystem::mkDir($_MG_CONF['path_mediaobjects'] . 'disp/' . $media_filename[0] . '/') === false ) {
+                Log::write('system',Log::ERROR,'MediaGallery: Unable to create directory: ' . $_MG_CONF['path_mediaobjects'] . 'disp/' . $media_filename[0] . '/');
+                $errMsg .= sprintf($LANG_MG02['move_error'],$filename);
+                return array(false,$errMsg);
+            }
+            if ( FileSystem::mkDir($_MG_CONF['path_mediaobjects'] . 'tn/' . $media_filename[0] . '/') === false ) {
+                Log::write('system',Log::ERROR,'MediaGallery: Unable to create directory: ' . $_MG_CONF['path_mediaobjects'] . 'tn/' . $media_filename[0] . '/');
+                $errMsg .= sprintf($LANG_MG02['move_error'],$filename);
+                return array(false,$errMsg);
             }
 
             if ( $_MG_CONF['verbose'] ) {
