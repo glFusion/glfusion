b2b6986e441668e7889578f770ff500577bd1e0b
Mark <mark@glfusion.org>
2021-04-11 22:05:08 -0500

    Media Gallery transition to /data/ storage

--- public_html/mediagallery/video.php
+++ public_html/mediagallery/video.php
@@ -1,31 +1,16 @@
 <?php
-// +--------------------------------------------------------------------------+
-// | Media Gallery Plugin - glFusion CMS                                      |
-// +--------------------------------------------------------------------------+
-// | video.php                                                                |
-// |                                                                          |
-// | Displays video in pop-up window                                          |
-// +--------------------------------------------------------------------------+
-// | Copyright (C) 2002-2017 by the following authors:                        |
-// |                                                                          |
-// | Mark R. Evans          mark AT glfusion DOT org                          |
-// +--------------------------------------------------------------------------+
-// |                                                                          |
-// | This program is free software; you can redistribute it and/or            |
-// | modify it under the terms of the GNU General Public License              |
-// | as published by the Free Software Foundation; either version 2           |
-// | of the License, or (at your option) any later version.                   |
-// |                                                                          |
-// | This program is distributed in the hope that it will be useful,          |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with this program; if not, write to the Free Software Foundation,  |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
-// |                                                                          |
-// +--------------------------------------------------------------------------+
+/**
+* glFusion CMS - Media Gallery Plugin
+*
+* Displays video in pop-up window
+*
+* @license GNU General Public License version 2 or later
+*     http://www.opensource.org/licenses/gpl-license.php
+*
+*  Copyright (C) 2002-2021 by the following authors:
+*   Mark R. Evans   mark AT glfusion DOT org
+*
+*/
 
 require_once '../lib-common.php';
 
@@ -683,7 +668,7 @@ if ( $nRows > 0 ) {
                     }
                 }
                 if ( !isset($media_size_orig) || $media_size_orig === false ) {
-                    $u_pic = $u_tn = $_MG_CONF['mediaobjects_url'] . '/placeholder_audio.svg';
+                    $u_pic = $u_tn = $_MG_CONF['assets_url'] . '/placeholder_audio.svg';
                     $playback_options['height'] = 200;
                     $playback_options['width'] = 300;
                 } else {
@@ -693,7 +678,7 @@ if ( $nRows > 0 ) {
                     $playback_options['width']  = 300;
                 }
             } else {
-                $u_pic = $u_tn = $_MG_CONF['mediaobjects_url'] . '/placeholder_audio.svg';
+                $u_pic = $u_tn = $_MG_CONF['assets_url'] . '/placeholder_audio.svg';
                 $playback_options['height'] = 200;
                 $playback_options['width'] = 300;
             }
