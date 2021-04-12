1284cf38985c1ab8f51dba47cdd6eff4b96866eb
Mark <mark@glfusion.org>
2021-04-12 08:33:03 -0500

    Create directory if it does not exist

--- private/system/classes/upload.class.php
+++ private/system/classes/upload.class.php
@@ -1,42 +1,26 @@
 <?php
-// +--------------------------------------------------------------------------+
-// | glFusion CMS                                                             |
-// +--------------------------------------------------------------------------+
-// | upload.class.php                                                         |
-// |                                                                          |
-// | glFusion file upload class library.                                      |
-// +--------------------------------------------------------------------------+
-// | Copyright (C) 2008-2015 by the following authors:                        |
-// |                                                                          |
-// | Mark R. Evans          mark AT glfusion DOT org                          |
-// |                                                                          |
-// | Copyright (C) 2000-2009 by the following authors:                        |
-// |                                                                          |
-// | Authors: Tony Bibbs       - tony AT tonybibbs DOT com                    |
-// |          Dirk Haun        - dirk AT haun-online DOT de                   |
-// +--------------------------------------------------------------------------+
-// |                                                                          |
-// | This program is free software; you can redistribute it and/or            |
-// | modify it under the terms of the GNU General Public License              |
-// | as published by the Free Software Foundation; either version 2           |
-// | of the License, or (at your option) any later version.                   |
-// |                                                                          |
-// | This program is distributed in the hope that it will be useful,          |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with this program; if not, write to the Free Software Foundation,  |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
-// |                                                                          |
-// +--------------------------------------------------------------------------+
-
+/**
+* glFusion CMS
+*
+* glFusion File Upload Class
+*
+* @license GNU General Public License version 2 or later
+*     http://www.opensource.org/licenses/gpl-license.php
+*
+*  Copyright (C) 2008-2021 by the following authors:
+*   Mark R. Evans   mark AT glfusion DOT org
+*
+*  Based on prior work Copyright (C) 2000-2009 by the following authors:
+*   Authors: Tony Bibbs  tony AT tonybibbs DOT com
+*            Dirk Haun   dirk AT haun-online DOT de
+*
+*/
 if (!defined ('GVERSION')) {
     die ('This file can not be used on its own.');
 }
 
 use \glFusion\Log\Log;
+use \glFusion\FileSystem;
 
 /**
 * This class will allow you to securely upload one or more files from a form
@@ -976,8 +960,10 @@ class upload
     function setPath($uploadDir)
     {
         if (!is_dir($uploadDir)) {
-            $this->_addError('Specified upload directory, ' . $uploadDir . ' is not a valid directory');
-            return false;
+            if (FileSystem::mkDir($uploadDir) === false) {
+                $this->_addError('Specified upload directory, ' . $uploadDir . ' is not a valid directory');
+                return false;
+            }
         }
 
         if (!is_writable($uploadDir)) {
