1b54891a9fdf92bff916f70c2786de2bf7321dc9
Mark <mark@glfusion.org>
2021-04-20 12:47:07 -0500

    Update COM_errorLog to Log class

--- private/plugins/captcha/upgrade.php
+++ private/plugins/captcha/upgrade.php
@@ -1,35 +1,22 @@
 <?php
-// +--------------------------------------------------------------------------+
-// | CAPTCHA Plugin - glFusion CMS                                            |
-// +--------------------------------------------------------------------------+
-// | upgrade.php                                                              |
-// |                                                                          |
-// | Upgrade routines                                                         |
-// +--------------------------------------------------------------------------+
-// | Copyright (C) 2005-2018 by the following authors:                        |
-// |                                                                          |
-// | Mark R. Evans          mark AT glfusion DOT org                          |
-// +--------------------------------------------------------------------------+
-// |                                                                          |
-// | This program is free software; you can redistribute it and/or            |
-// | modify it under the terms of the GNU General Public License              |
-// | as published by the Free Software Foundation; either version 2           |
-// | of the License, or (at your option) any later version.                   |
-// |                                                                          |
-// | This program is distributed in the hope that it will be useful,          |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with this program; if not, write to the Free Software Foundation,  |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
-// |                                                                          |
-// +--------------------------------------------------------------------------+
+/**
+* glFusion CMS - CAPTCHA Plugin
+*
+* Upgrade
+*
+* @license GNU General Public License version 2 or later
+*     http://www.opensource.org/licenses/gpl-license.php
+*
+*  Copyright (C) 2002-2021 by the following authors:
+*   Mark R. Evans   mark AT glfusion DOT org
+*
+*/
 
 if (!defined ('GVERSION')) {
     die ('This file can not be used on its own.');
 }
+
+use \glFusion\Log\Log;
 
 require_once $_CONF['path'].'plugins/captcha/captcha.php';
 
@@ -57,12 +44,12 @@ function captcha_upgrade()
                             " );";
 
             foreach ($_SQL as $table => $sql) {
-                COM_errorLog("Creating $table table",1);
+                Log::write('system',Log::INFO,'Creating '.$table.' table');
                 DB_query($sql,1);
                 if (DB_error()) {
-                    COM_errorLog("Error Creating $table table",1);
+                    Log::write('system',Log::ERROR, 'Error Creating '.$table.' table');
                 }
-                COM_errorLog("Success - Created $table table",1);
+                Log::write('system',Log::INFO,'Success - Created '.$table.' table');
             }
             DB_query("UPDATE {$_TABLES['plugins']} SET pi_version='3.0.0' WHERE pi_name='captcha' LIMIT 1");
         case "3.0.0" :
