0ac1b9be8d57d030211b65c46bb4c2a99187091f
Mark <mark@glfusion.org>
2021-04-20 19:17:11 -0500

    Update COM_errorLog to Log class

--- public_html/admin/plugins/links/install.php
+++ public_html/admin/plugins/links/install.php
@@ -1,47 +1,36 @@
 <?php
-// +--------------------------------------------------------------------------+
-// | Links Plugin - glFusion CMS                                              |
-// +--------------------------------------------------------------------------+
-// | install.php                                                              |
-// |                                                                          |
-// | This file installs and removes the data structures for the               |
-// | Links plugin for glFusion.                                               |
-// +--------------------------------------------------------------------------+
-// | Copyright (C) 2000-2008 by the following authors:                        |
-// |                                                                          |
-// | Authors: Tony Bibbs         - tony AT tonybibbs DOT com                  |
-// |          Tom Willett        - tom AT pigstye DOT net                     |
-// |          Blaine Lang        - blaine AT portalparts DOT com              |
-// |          Dirk Haun          - dirk AT haun-online DOT de                 |
-// |          Vincent Furia      - vinny01 AT users DOT sourceforge DOT net   |
-// |          Oliver Spiesshofer - oliver AT spiesshofer DOT com              |
-// |          Euan McKay         - info AT heatherengineering DOT com         |
-// +--------------------------------------------------------------------------+
-// |                                                                          |
-// | This program is free software; you can redistribute it and/or            |
-// | modify it under the terms of the GNU General Public License              |
-// | as published by the Free Software Foundation; either version 2           |
-// | of the License, or (at your option) any later version.                   |
-// |                                                                          |
-// | This program is distributed in the hope that it will be useful,          |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with this program; if not, write to the Free Software Foundation,  |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
-// |                                                                          |
-// +--------------------------------------------------------------------------+
+/**
+* glFusion CMS - Links Plugin
+*
+* Installation
+*
+* @license GNU General Public License version 2 or later
+*     http://www.opensource.org/licenses/gpl-license.php
+*
+*  Copyright (C) 2012-2021 by the following authors:
+*   Mark R. Evans   mark AT glfusion DOT org
+*
+*  Based on prior work Copyright (C) 2000-2008 by the following authors:
+*  Tony Bibbs         tony AT tonybibbs DOT com
+*  Tom Willett        tom AT pigstye DOT net
+*  Blaine Lang        blaine AT portalparts DOT com
+*  Dirk Haun          dirk AT haun-online DOT de
+*  Vincent Furia      vinny01 AT users DOT sourceforge DOT net
+*  Oliver Spiesshofer oliver AT spiesshofer DOT com
+*  Euan McKay         info AT heatherengineering DOT com
+*
+*/
 
 require_once '../../../lib-common.php';
 require_once $_CONF['path'].'/plugins/links/autoinstall.php';
+
+use \glFusion\Log\Log;
 
 USES_lib_install();
 
 if (!SEC_inGroup('Root')) {
     // Someone is trying to illegally access this page
-    COM_errorLog("Someone has tried to illegally access the Links install/uninstall page.  User id: {$_USER['uid']}, Username: {$_USER['username']}, IP: $REMOTE_ADDR",1);
+    Log::write('system',Log::ERROR,'Someone has tried to access the Links install/uninstall page.  User id: '.$_USER['uid'].', IP: ' . $_SERVER['REAL_ADDR']);
     $display = COM_siteHeader ('menu', $LANG_ACCESS['accessdenied'])
              . COM_startBlock ($LANG_ACCESS['accessdenied'])
              . $LANG_ACCESS['plugin_access_denied_msg']
