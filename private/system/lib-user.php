a1d2d12f2dbe819c413a51540d78dc03084912a3
mark <mark@glfusion.org>
2021-06-08 11:29:14 -0500

    Send to site email

--- private/system/lib-user.php
+++ private/system/lib-user.php
@@ -668,7 +668,7 @@ function USER_sendNotification ($username, $email, $uid, $mode='inactive')
     $mailsubject = $_CONF['site_name'] . ' ' . $LANG29[40];
 
     $to = array();
-    $to   = COM_formatEmailAddress( '',$_CONF['noreply_mail'] );
+    $to   = COM_formatEmailAddress( '',$_CONF['site_mail']);
     COM_mail ($to, $mailsubject, $mailbody);
 }
 
