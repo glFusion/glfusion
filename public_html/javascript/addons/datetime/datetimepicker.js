59c65d2c867a21881370b6869d0b5a0dbfa77c63
Lee Garner <lee@leegarner.com>
2021-08-09 08:57:19 -0700

    Enable date-only and time-only pickers (#453)

--- public_html/javascript/addons/datetime/datetimepicker.js
+++ public_html/javascript/addons/datetime/datetimepicker.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
 		var id = $(this).attr('id');
 		datetimepicker_popupcal(id);
 	});
+	$('.datepicker').each(function(i, obj) {
+		var id = $(this).attr('id');
+		datetimepicker_datepicker(id);
+	});
+	$('.timepicker').each(function(i, obj) {
+		var id = $(this).attr('id');
+		datetimepicker_timepicker(id);
+	});
 });
 function datetimepicker_popupcal( selector ) {
 	var currentDT = $("#"+selector).val();
@@ -12,5 +20,26 @@ function datetimepicker_popupcal( selector ) {
 		lazyInit: true,
 		value:currentDT,
 		format:'Y-m-d H:i',
+	});
+}
+function datetimepicker_datepicker( selector ) {
+	var currentDT = $("#"+selector).val();
+	$('#'+selector).val( currentDT );
+	$('#'+selector).datetimepicker({
+		lazyInit: true,
+		value:currentDT,
+		format:'Y-m-d',
+		timepicker: false,
+	});
+}
+function datetimepicker_timepicker( selector ) {
+	var currentDT = $("#"+selector).val();
+	$('#'+selector).val( currentDT );
+	$('#'+selector).datetimepicker({
+		lazyInit: true,
+		value:currentDT,
+		format:'H:i',
+		datepicker: false,
+		step: 15,
 	});
 }
