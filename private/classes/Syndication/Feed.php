92532a7e5bf96bcd9047c7700cf3afb01184c094
Lee Garner <lee@leegarner.com>
2022-01-13 08:09:49 -0800

    Sanitize properties, remove extra code copied from other class (#497)

--- private/classes/Syndication/Feed.php
+++ private/classes/Syndication/Feed.php
@@ -165,6 +165,7 @@ class Feed
     {
         global $_TABLES;
 
+        $retval = array();
         $db = Database::getInstance();
         $sql = "SELECT * FROM `{$_TABLES['syndication']}`";
         if (!empty($where)) {
@@ -215,26 +216,34 @@ class Feed
     {
         global $_CONF;
 
-        // All property names, except "updated" which gets a Date object
+        // All property names
         $fields = array(
-            'fid', 'type', 'topic', 'header_tid', 'limits', //'format', 
+            'fid', 'type', 'topic', 'header_tid', 'limits', 'format',
             'content_length', 'title', 'description', 'feedlogo', 'filename',
-            'charset', 'language', 'is_enabled', 'update_info',
+            'charset', 'language', 'is_enabled', 'update_info', 'updated',
         );
         foreach ($fields as $fld_name) {
             if (isset($A[$fld_name])) {
-                $this->$fld_name = $A[$fld_name];
+                switch ($fld_name) {
+                case 'is_enabled':
+                case 'content_length':
+                    $this->$fld_name = (int)$A[$fld_name];
+                    break;
+                case 'format':
+                    $format = explode('-', $A['format']);
+                    $this->format = $format[0];
+                    if (isset($format[1])) {
+                        $this->format_version = $format[1];
+                    }
+                    break;
+                case 'updated':
+                    $this->updated = new \Date($A['updated'], $_CONF['timezone']);
+                    break;
+                default:
+                    $this->$fld_name = $A[$fld_name];
+                    break;
+                }
             }
-        }
-        if (isset($A['format'])) {
-            $format = explode('-', $A['format']);
-            $this->format = $format[0];
-            if (isset($format[1])) {
-                $this->format_version = $format[1];
-            }
-        }
-        if (isset($A['updated'])) {
-            $this->updated = new \Date($A['updated'], $_CONF['timezone']);
         }
         return $this;
     }
@@ -309,7 +318,8 @@ class Feed
         $feed = $_CONF['path_rss'] . $feedfile;
         return $feed;
     }
-    
+
+
     /**
      * Get the URL of the feed directory or a specific feed file
      *
@@ -379,6 +389,7 @@ class Feed
     /**
      * Get all the available feed formats.
      *
+     * @param   string  $type   Type of feed, e.g. plugin name
      * @return  array   Array of feed (name, version) elements
      */
     public static function findFormats(string $type = '') : array
@@ -431,326 +442,6 @@ class Feed
             Log:;write('system', Log::ERROR, "Error: Unable to open $filepath for writing");
             return false;
         }
-    }
-
-
-    /**
-     * Creates the edit form.
-     *
-     * @return  string      HTML for edit form
-     */
-    public function Edit()
-    {
-        global $_CONF, $LANG_ADMIN, $LANG_ACCESS, $_TABLES;
-
-        $allgroups = \Group::getAllAvailable();
-        $inc_groups = self::Groups($this->ft_name);
-
-        $included_opts = '';
-        $excluded_opts = '';
-        foreach ($allgroups as $grp_name=>$grp_id) {
-            if (in_array($grp_id, $inc_groups)) {
-                $included_opts .= '<option value="' . $grp_id . '">' . $grp_name . '</option>' . LB;
-            } else {
-                $excluded_opts .= '<option value="' . $grp_id . '">' . $grp_name . '</option>' . LB;
-            }
-        }
-
-        $T = new \Template($_CONF['path_layout'] . '/admin/feature');
-        $T->set_file('form', 'featureeditor.thtml');
-        $T->set_var(array(
-            'fid'         => $this->fid,
-            'ft_name'       => $this->ft_name,
-            'ft_descr'      => $this->ft_descr,
-            'ft_gl_core'    => $this->ft_gl_core,
-            'lang_fid'    => $LANG_ACCESS['feature_id'],
-            'lang_ft_name'  => $LANG_ACCESS['feature_name'],
-            'lang_ft_descr' => $LANG_ACCESS['description'],
-            'lang_fg_gl_core' => $LANG_ACCESS['coregroup'],
-            'lang_save'     => $LANG_ADMIN['save'],
-            'lang_cancel'   => $LANG_ADMIN['cancel'],
-            'gltoken_name'  => CSRF_TOKEN,
-            'gltoken'       => SEC_createToken(),
-            'included_groups' => $included_opts,
-            'excluded_groups' => $excluded_opts,
-            'lang_available' => $LANG_ACCESS['avail_groups'],
-            'lang_included' => $LANG_ADMIN['assigned_groups'],
-            'LANG_add'      => $LANG_ACCESS['add'],
-            'LANG_remove'   => $LANG_ACCESS['remove'],
-        ) );
-
-        // Construct the checkboxes of groups
-        /*$groups = $this->getGroups();
-        $sql = "SELECT grp_id, grp_name, grp_descr FROM {$_TABLES['groups']}";
-        $res = DB_query($sql);
-        $T->set_block('form', 'grpItems', 'grpChk');
-        while ($A = DB_fetchArray($res, false)) {
-            $T->set_var(array(
-                'grp_id'    => $A['grp_id'],
-                'grp_name'  => $A['grp_name'],
-                'grp_descr' => $A['grp_descr'],
-                'chk'       => in_array($A['grp_id'], $groups) ? 'checked="checked"' : '',
-            ) );
-            $T->parse('grpChk', 'grpItems', true);
-        }*/
-
-        $T->parse('output', 'form');
-        $retval = COM_startBlock(
-            $LANG_ADMIN['feature_editor'],
-            '',
-            COM_getBlockTemplate('_admin_block', 'header')
-        );
-        $retval .= self::adminMenu('edit');
-        $retval .= $T->finish($T->get_var('output'));
-        $retval .= COM_endBlock(COM_getBlockTemplate('_admin_block', 'footer'));
-        return $retval;
-    }
-
-
-    /**
-     * Save the feature information to the database.
-     *
-     * @param   array   $A      Optional array of values from $_POST
-     * @return  boolean         True if no errors, False otherwise
-     */
-    public function Save($A =NULL)
-    {
-        global $_TABLES, $_SHOP_CONF;
-
-        if (is_array($A)) {
-            $this->setVars($A, false);
-        }
-
-        $db = Database::getInstance();
-        $dataArray = array();
-        $typeArray = array();
-
-        // Insert or update the record, as appropriate.
-        if ($this->fid == 0) {
-            $isNew = true;
-            $sql1 = "INSERT INTO `{$_TABLES['features']}`";
-            $sql3 = '';
-        } else {
-            $isNew = false;
-            $sql1 = "UPDATE `{$_TABLES['features']}`";
-            $sql3 = " WHERE fid = ?";
-            $dataArray[] = $this->fid;
-            $typeArray[] = DATABASE::INTEGER;
-        }
-        $sql2 = " SET ft_name = ?,
-            ft_descr = ?,
-            ft_gl_core  = ?";
-        $dataArray[] = $this->ft_name;
-        $typeArray[] = DATABASE::STRING;
-
-        $dataArray[] = $this->ft_descr;
-        $typeArray[] = DATABASE::STRING;
-
-        $dataArray[] = $this->ft_gl_core;
-        $typeArray[] = DATABASE::INTEGER;
-
-        $sql = $sql1 . $sql2 . $sql3;
-        //echo $sql;die;
-
-        $status = true;
-        try {
-            $stmt = $db->conn->executeQuery($sql,$dataArray,$typeArray);
-        } catch(Throwable $e) {
-            $status = false;
-        }
-        if ($isNew) {
-            $this->fid = $db->conn->lastInsertId();
-        }
-
-        // Now save the group assignments
-        if ($status) {
-            if (!$isNew) {
-                // If updating, delete all access records.
-                // Simplest method to remove any unchecked groups.
-
-                try {
-                    $db->conn->delete($_TABLES['access'],array('acc_fid' => $this->fid),array(DATABASE::INTEGER));
-                } catch(Throwable $e) {
-                    throw($e);
-                }
-            }
-            if (isset($A['groupmembers'])) {
-                $grp_members = explode('|', $A['groupmembers']);
-                foreach (explode('|', $A['groupmembers']) as $grp_id) {
-                    $vals[] = "(" . intval($this->fid).",".intval($grp_id).")";
-                }
-                $vals = implode(', ', $vals);
-                $sql = "INSERT INTO `{$_TABLES['access']}` VALUES $vals";
-                try {
-                    $db->conn->query($sql);
-                } catch (Throwable $e) {
-                    throw($e);
-                }
-            }
-        }
-        return $status;
-    }
-
-
-    /**
-     * Displays the admin list of features.
-     *
-     * @return  string      List of features
-     */
-    public static function adminList($coreonly=0)
-    {
-        global $_CONF, $_TABLES, $LANG_ADMIN, $LANG_ACCESS, $LANG28;
-
-        USES_lib_admin();
-        $retval = '';
-
-        $sql = "SELECT * FROM `{$_TABLES['features']}` ";
-        $header_arr = array(
-            array(
-                'text'  => 'ID',
-                'field' => 'fid',
-                'sort'  => true,
-            ),
-            array(
-                'text'  => $LANG_ADMIN['edit'],
-                'field' => 'edit',
-                'sort'  => false,
-                'align' => 'center',
-            ),
-            array(
-                'text'  => $LANG_ADMIN['name'],
-                'field' => 'ft_name',
-                'sort'  => true,
-            ),
-            array(
-                'text'  => $LANG_ACCESS['description'],
-                'field' => 'ft_descr',
-            ),
-            array(
-                'text'  => $LANG_ACCESS['coregroup'],
-                'field' => 'ft_gl_core',
-            ),
-        );
-
-        $defsort_arr = array(
-            'field' => 'ft_name',
-            'direction' => 'ASC',
-        );
-
-        if ($coreonly) {
-            $def_filter = ' WHERE ft_gl_core = 1';
-            $corechk = 'checked="checked"';
-        } else {
-            $def_filter = ' WHERE 1=1';
-            $corechk = '';
-        }
-
-        $filter = '<input type="checkbox" name="core" ' . $corechk .
-            ' onclick="this.form.submit();">&nbsp;' .
-            $LANG_ADMIN['core_only'] . '&nbsp;&nbsp;';
-
-        $query_arr = array(
-            'table' => 'features',
-            'sql' => $sql,
-            'query_fields' => array('ft_name', 'ft_descr'),
-            'default_filter' => $def_filter,
-        );
-        $text_arr = array(
-            'has_extras' => true,
-            'form_url' => $_CONF['site_admin_url'] . '/feature.php',
-        );
-
-        $options = array();
-        $retval .= COM_startBlock(
-            $LANG_ADMIN['feature_admin'],
-            '',
-            COM_getBlockTemplate('_admin_block', 'header')
-        );
-        $retval .= self::adminMenu('list');
-        $retval .= ADMIN_list(
-            'gl_ft_adminlist',
-            array(__CLASS__,  'getAdminField'),
-            $header_arr, $text_arr, $query_arr, $defsort_arr,
-            $filter, '', $options, ''
-        );
-        $retval .= COM_endBlock(COM_getBlockTemplate('_admin_block', 'footer'));
-        return $retval;
-    }
-
-
-    /**
-     * Get an individual field for the feature list.
-     *
-     * @param  string  $fieldname  Name of field (from the array, not the db)
-     * @param  mixed   $fieldvalue Value of the field
-     * @param  array   $A          Array of all fields from the database
-     * @param  array   $icon_arr   System icon array (not used)
-     * @return string              HTML for field display in the table
-     */
-    public static function getAdminField($fieldname, $fieldvalue, $A, $icon_arr)
-    {
-        global $_CONF, $LANG_ADMIN;
-
-        static $grp_names = array();
-        $retval = '';
-
-        switch($fieldname) {
-        case 'edit':
-            $retval = FieldList::edit(array(
-                'url' => $_CONF['site_admin_url'] . "/feature.php?edit={$A['fid']}",
-                'attr' => array(
-                    'title' => $LANG_ADMIN['edit']
-                )
-            ) );
-            break;
-        case 'ft_gl_core':
-            if ($fieldvalue) {
-                $retval = FieldList::checkmark(array(
-                    'active' => true,
-                ) );
-            }
-            break;
-        default:
-            $retval = htmlspecialchars($fieldvalue, ENT_QUOTES, COM_getEncodingt());
-            break;
-        }
-        return $retval;
-    }
-
-
-    /**
-     * Create a menu for the admin interface.
-     *
-     * @param   string  $view   View to mark as active
-     * @return  string      HTML for admin menu
-     */
-    public static function adminMenu($view='list')
-    {
-        global $_CONF, $LANG_ADMIN, $LANG_ACCESS;
-
-        USES_lib_admin();
-
-        $menu_arr = array(
-            array(
-                'url' => $_CONF['site_admin_url'] . '/feature.php',
-                'text' => $LANG_ADMIN['feature_list'],
-                'active'=> $view == 'list',
-            ),
-            array(
-                'url' => $_CONF['site_admin_url'] . '/group.php',
-                'text' => $LANG_ADMIN['admin_groups'],
-            ),
-            array(
-                'url' => $_CONF['site_admin_url'].'/index.php',
-                'text' => $LANG_ADMIN['admin_home'],
-            ),
-        );
-        $retval = ADMIN_createMenu(
-            $menu_arr,
-            '',
-            $_CONF['layout_url'] . '/images/icons/group.png',
-        );
-        return $retval;
     }
 
 }
