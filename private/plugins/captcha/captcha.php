0b98a3d2e54ba3382ad5391b1957f3237d7fb007
mark <mark@glfusion.org>
2022-03-01 18:59:01 -0600

    Do not show CAPTCHA for Root users if force for remote users is enabled Bump version to 3.7.1

--- private/plugins/captcha/captcha.php
+++ private/plugins/captcha/captcha.php
@@ -1,31 +1,16 @@
 <?php
-// +--------------------------------------------------------------------------+
-// | CAPTCHA Plugin - glFusion CMS                                            |
-// +--------------------------------------------------------------------------+
-// | captcha.php                                                              |
-// |                                                                          |
-// | Plugin system integration options                                        |
-// +--------------------------------------------------------------------------+
-// | Copyright (C) 2009-2018 by the following authors:                        |
-// |                                                                          |
-// | Mark R. Evans          mark AT glfusion DOT org                          |
-// +--------------------------------------------------------------------------+
-// |                                                                          |
-// | This program is free software; you can redistribute it and/or            |
-// | modify it under the terms of the GNU General Public License              |
-// | as published by the Free Software Foundation; either version 2           |
-// | of the License, or (at your option) any later version.                   |
-// |                                                                          |
-// | This program is distributed in the hope that it will be useful,          |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with this program; if not, write to the Free Software Foundation,  |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
-// |                                                                          |
-// +--------------------------------------------------------------------------+
+/**
+* glFusion CMS
+*
+* CAPTCHA Plugin
+*
+* @license GNU General Public License version 2 or later
+*     http://www.opensource.org/licenses/gpl-license.php
+*
+*  Copyright (C) 2002-2022 by the following authors:
+*   Mark R. Evans   mark AT glfusion DOT org
+*
+*/
 
 if (!defined ('GVERSION')) {
     die ('This file can not be used on its own.');
@@ -37,7 +22,7 @@ global $_DB_table_prefix, $_TABLES, $_CP_CONF;
 
 $_CP_CONF['pi_name']            = 'captcha';
 $_CP_CONF['pi_display_name']    = 'CAPTCHA';
-$_CP_CONF['pi_version']         = '3.7.0';
+$_CP_CONF['pi_version']         = '3.7.1';
 $_CP_CONF['gl_version']         = '2.0.1';
 $_CP_CONF['pi_url']             = 'https://www.glfusion.org/';
 
