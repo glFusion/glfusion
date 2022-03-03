939f206a46e262c78debd17bf7ab6a2ace37291c
mark <mark@glfusion.org>
2022-03-03 06:15:32 -0600

    Configuration changes  - changed fillEmptyBlocks to true to prevent removing <p></p> blocks  - Ensure both glFusion and CKEditor style sheets are loaded  - Removed redundant configuration for some styles

--- public_html/ckeditor/config.js
+++ public_html/ckeditor/config.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2022, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 var tbitems = {
@@ -47,7 +47,7 @@ CKEDITOR.editorConfig = function( config ) {
 		tbitems.items['tools'],
 		tbitems.items['about']
 	];
-	config.font_defaultLabel = 'Arial';
+	config.font_defaultLabel = 'Default';
 	config.fontSize_defaultLabel = '14px';
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
