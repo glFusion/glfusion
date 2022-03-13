37e8b7deb43721ee8e059512c72e21fa05951552
mark <mark@glfusion.org>
2022-03-13 15:30:35 -0500

    Added missing files

---
+++ private/classes/Auth/ConfirmationRequestNotFound.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * PHP-Auth (https://github.com/delight-im/PHP-Auth)
+ * Copyright (c) delight.im (https://www.delight.im/)
+ * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+ */
+
+namespace glFusion\Auth;
+
+if (!defined ('GVERSION')) {
+    die ('This file can not be used on its own.');
+}
+
+class ConfirmationRequestNotFound extends AuthException {}
