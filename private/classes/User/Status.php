55721ef4146d13e197d443521b0a13268ff10fb3
mark <mark@glfusion.org>
2022-04-08 10:59:40 -0500

    Formatting cleanup

--- private/classes/User/Status.php
+++ private/classes/User/Status.php
@@ -17,12 +17,11 @@ define('USER_ACCOUNT_AWAITING_VERIFICATION', 4); // Account waiting for user to 
 */
 
 final class Status {
-
-	const NORMAL 	= 3;	// USER_ACCOUNT_ACTIVE (3) or USER_ACCOUNT_AWAITING_ACTIVATION (1)
-	const ARCHIVED 	= 5;	// not mapped
-	const BANNED 	= 0;	// USER_ACCOUNT_DISABLED (0)
-	const LOCKED 	= 6;	// not mapped - new
-	const PENDING_REVIEW = 2;	// USER_ACCOUNT_AWAITING_APPROVAL (2)
-	const SUSPENDED = 7;	// not mapped
+    const NORMAL 	= 3;	// USER_ACCOUNT_ACTIVE (3) or USER_ACCOUNT_AWAITING_ACTIVATION (1)
+    const ARCHIVED 	= 5;	// not mapped
+    const BANNED 	= 0;	// USER_ACCOUNT_DISABLED (0)
+    const LOCKED 	= 6;	// not mapped - new
+    const PENDING_REVIEW = 2;	// USER_ACCOUNT_AWAITING_APPROVAL (2)
+    const SUSPENDED = 7;	// not mapped
 
 }
