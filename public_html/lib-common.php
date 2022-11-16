9c5152f7ff49dc959880e3538eb58ee9fe2466f5
Lee Garner <lee@leegarner.com>
2022-11-16 08:54:59 -0800

    Fix #560, never show output for blocks with no content (#562)

--- public_html/lib-common.php
+++ public_html/lib-common.php
@@ -2891,12 +2891,14 @@ function COM_formatBlock( $A, $noboxes = false)
         if (( $A['allow_autotags'] == 1 ) && ( $A['type'] == 'normal' )) {
             $blockcontent = PLG_replaceTags( $blockcontent,'glfusion','block' );
         }
-        $blockcontent = str_replace( array( '<?', '?>' ), '', $blockcontent );
+        $blockcontent = str_replace( array( '<'.'?', '?'.'>' ), '', $blockcontent );
 
-        $retval .= COM_startBlock( $A['title'], $A['help'],
+        if (!empty($blockcontent)) {
+            $retval .= COM_startBlock( $A['title'], $A['help'],
                        COM_getBlockTemplate( $A['name'], 'header', $position ), $A['name'] )
                 . $blockcontent . PHP_EOL
                 . COM_endBlock( COM_getBlockTemplate( $A['name'], 'footer', $position ));
+        }
     }
 
     return $retval;
